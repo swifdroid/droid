0a4a61e18896bb184a260c243f45512f8982dad3
Mikhail Isaev <isaev.mihael@gmail.com>
2025-07-25 22:53:56 +0400

    🪚 Manifest as inner target

--- Package.swift
+++ Package.swift
@@ -8,9 +8,7 @@ let package = Package(
         .library(name: "Droid", targets: ["Droid"]),
     ],
     dependencies: [
-        .package(path: "/Users/imike/Development/SwifDroid/manifest"),
         .package(path: "/Users/imike/Development/SwifDroid/jni-kit")
-//        .package(url: "https://github.com/SwifDroid/manifest.git", from: "0.0.1"),
 //        .package(url: "https://github.com/SwifDroid/jni-kit.git", from: "1.0.0"),
     ],
     targets: [
@@ -19,8 +17,9 @@ let package = Package(
         ]),
         .target(name: "Droid", dependencies: [
             .product(name: "JNIKit", package: "jni-kit"),
-            .product(name: "Manifest", package: "manifest")
+            .target(name: "Manifest"),
         ]),
+        .target(name: "Manifest"),
         .testTarget(name: "DroidTests", dependencies: ["Droid"]),
     ]
 )
