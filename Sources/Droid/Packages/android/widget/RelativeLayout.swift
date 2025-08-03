c2f1515f6e3267c52bcca576eaa147f21991197a
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-03 15:13:46 +0400

    🪚 RelativeLayout: add `LayoutParams.Class`

--- Sources/Droid/Packages/android/widget/RelativeLayout.swift
+++ Sources/Droid/Packages/android/widget/RelativeLayout.swift
@@ -9,10 +9,18 @@ extension AndroidPackage.WidgetPackage {
     public class RelativeLayoutClass: JClassName, @unchecked Sendable {}
     public var RelativeLayout: RelativeLayoutClass { .init(parent: self, name: "RelativeLayout") }
 }
+extension AndroidPackage.WidgetPackage.RelativeLayoutClass {
+    public class LayoutParamsClass: JClassName, @unchecked Sendable {}
+    public var LayoutParams: LayoutParamsClass { .init(parent: self, name: "LayoutParams", isInnerClass: true) }
+}
+extension LayoutParams.Class {
+    static let relativeLayout: Self = .init(.android.widget.RelativeLayout.LayoutParams)
+}
 
 open class RelativeLayout: View, @unchecked Sendable {
     /// The JNI class name
     public override class var className: JClassName { .android.widget.RelativeLayout }
+    public override class var layoutParamsClass: LayoutParams.Class { .relativeLayout }
 
     @discardableResult
     public override init() {
