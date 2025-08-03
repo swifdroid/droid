38d83be68fcf797cc9c2af2b6be7c191027c5cdd
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-03 15:12:08 +0400

    🪚 ViewGroup: add `LayoutParams.Class`

--- Sources/Droid/Packages/android/view/ViewGroup.swift
+++ Sources/Droid/Packages/android/view/ViewGroup.swift
@@ -9,6 +9,13 @@ extension AndroidPackage.ViewPackage {
     public class ViewGroupClass: JClassName, @unchecked Sendable {}
     public var ViewGroup: ViewGroupClass { .init(parent: self, name: "ViewGroup") }
 }
+extension AndroidPackage.ViewPackage.ViewGroupClass {
+    public class LayoutParamsClass: JClassName, @unchecked Sendable {}
+    public var LayoutParams: LayoutParamsClass { .init(parent: self, name: "LayoutParams", isInnerClass: true) }
+}
+extension LayoutParams.Class {
+    public static let viewGroup: Self = .init(.android.view.ViewGroup.LayoutParams)
+}
 
 open class ViewGroup: View, @unchecked Sendable {
     /// The JNI class name
