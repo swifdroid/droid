519df892d53fb20ae9e400bf19cdb103f06c190f
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-03 15:12:24 +0400

    🪚 AbsoluteLayout: add `LayoutParams.Class`

--- Sources/Droid/Packages/android/widget/AbsoluteLayout.swift
+++ Sources/Droid/Packages/android/widget/AbsoluteLayout.swift
@@ -9,10 +9,18 @@ extension AndroidPackage.WidgetPackage {
     public class AbsoluteLayoutClass: JClassName, @unchecked Sendable {}
     public var AbsoluteLayout: AbsoluteLayoutClass { .init(parent: self, name: "AbsoluteLayout") }
 }
+extension AndroidPackage.WidgetPackage.AbsoluteLayoutClass {
+    public class LayoutParamsClass: JClassName, @unchecked Sendable {}
+    public var LayoutParams: LayoutParamsClass { .init(parent: self, name: "LayoutParams", isInnerClass: true) }
+}
+extension LayoutParams.Class {
+    static let absoluteLayout: Self = .init(.android.widget.AbsoluteLayout.LayoutParams)
+}
 
 open class AbsoluteLayout: ViewGroup, @unchecked Sendable {
     /// The JNI class name
     open override class var className: JClassName { .android.widget.AbsoluteLayout }
+    open override class var layoutParamsClass: LayoutParams.Class { .absoluteLayout }
 
     @discardableResult
     public override init() {
