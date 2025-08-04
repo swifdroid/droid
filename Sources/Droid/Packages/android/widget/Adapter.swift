4d8e6743879e1e22997ae3e45e3a466720f92c12
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 07:02:36 +0400

    🪚 Implement `Adapter` protocol

---
+++ Sources/Droid/Packages/android/widget/Adapter.swift
@@ -0,0 +1,23 @@
+//
+//  AbsSeekBar.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 05.08.2025.
+//
+
+public protocol Adapter {
+    // static final int IGNORE_ITEM_VIEW_TYPE = AdapterView.ITEM_VIEW_TYPE_IGNORE;
+    // static final int NO_SELECTION = Integer.MIN_VALUE;
+
+    // void registerDataSetObserver(DataSetObserver observer);
+    // void unregisterDataSetObserver(DataSetObserver observer);
+    // int getCount();
+    // Object getItem(int position);
+    // long getItemId(int position);
+    // boolean hasStableIds();
+    // View getView(int position, View convertView, ViewGroup parent);
+    // int getItemViewType(int position);
+    // int getViewTypeCount();
+    // boolean isEmpty();
+}
+// TODO: create android.widget.Adapter interface as NativeAdapter like NativeOnClickListener
