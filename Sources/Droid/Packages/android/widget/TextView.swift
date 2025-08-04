24c9a6ceec3ace4ea65b3fb76ac6d43602968333
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 05:00:28 +0400

    🪚 TextView: initial implementation

--- Sources/Droid/Packages/android/widget/TextView.swift
+++ Sources/Droid/Packages/android/widget/TextView.swift
@@ -1,22 +1,21 @@
-// //
-// //  TextView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 15.01.2022.
-// //
+//
+//  TextView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 15.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class TextViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var TextView: TextViewClass { .init(parent: self, name: "TextView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class TextViewClass: JClassName, @unchecked Sendable {}
+    public var TextView: TextViewClass { .init(parent: self, name: "TextView") }
+}
 
-// class TextView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.TextView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class TextView: View, @unchecked Sendable {
+    /// The JNI class name
+    open override class var className: JClassName { .android.widget.TextView }
+
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
