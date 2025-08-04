e7cc0d3971bab36f1884c2ede9e3c24378f8753b
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 06:42:19 +0400

    🪚 GridView: initial implementation

--- Sources/Droid/Packages/android/widget/GridView.swift
+++ Sources/Droid/Packages/android/widget/GridView.swift
@@ -1,22 +1,18 @@
-// //
-// //  GridView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  GridView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class GridViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var GridView: GridViewClass { .init(parent: self, name: "GridView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class GridViewClass: JClassName, @unchecked Sendable {}
+    public var GridView: GridViewClass { .init(parent: self, name: "GridView") }
+}
 
-// class GridView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.GridView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class GridView: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
