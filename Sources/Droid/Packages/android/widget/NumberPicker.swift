ac8713b6271cea383042196f8951a4b78a074014
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 07:36:10 +0400

    🪚 NumberPicker: initial implementation

--- Sources/Droid/Packages/android/widget/NumberPicker.swift
+++ Sources/Droid/Packages/android/widget/NumberPicker.swift
@@ -1,22 +1,18 @@
-// //
-// //  NumberPicker.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  NumberPicker.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class NumberPickerClass: JClassName, @unchecked Sendable {}
-    
-//     public var NumberPicker: NumberPickerClass { .init(parent: self, name: "NumberPicker") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class NumberPickerClass: JClassName, @unchecked Sendable {}
+    public var NumberPicker: NumberPickerClass { .init(parent: self, name: "NumberPicker") }
+}
 
-// class NumberPicker: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.NumberPicker], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class NumberPicker: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
