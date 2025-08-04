8c694c90417acfed8c47a66b966550780359a00a
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 06:41:49 +0400

    🪚 CompoundButton: initial implementation

---
+++ Sources/Droid/Packages/android/widget/CompoundButton.swift
@@ -0,0 +1,18 @@
+//
+//  CompoundButton.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 05.08.2025.
+//
+
+extension AndroidPackage.WidgetPackage {
+    public class CompoundButtonClass: JClassName, @unchecked Sendable {}
+    public var CompoundButton: CompoundButtonClass { .init(parent: self, name: "CompoundButton") }
+}
+
+open class CompoundButton: Button, Checkable, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
