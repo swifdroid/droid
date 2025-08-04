79cbab0b02ba534f53eb86774d7f3b6a0327d395
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 04:24:36 +0400

    🪚 AbsSeekBar: initial implementation

---
+++ Sources/Droid/Packages/android/widget/AbsSeekBar.swift
@@ -0,0 +1,13 @@
+//
+//  AbsSeekBar.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 05.08.2025.
+//
+
+open class AbsSeekBar: ProgressBar, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
