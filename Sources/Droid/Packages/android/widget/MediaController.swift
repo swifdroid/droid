69a433b442c96afc106997a0b9ef3613de34a331
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 07:35:57 +0400

    🪚 MediaController: initial implementation

--- Sources/Droid/Packages/android/widget/MediaController.swift
+++ Sources/Droid/Packages/android/widget/MediaController.swift
@@ -1,22 +1,18 @@
-// //
-// //  MediaController.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  MediaController.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class MediaControllerClass: JClassName, @unchecked Sendable {}
-    
-//     public var MediaController: MediaControllerClass { .init(parent: self, name: "MediaController") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class MediaControllerClass: JClassName, @unchecked Sendable {}
+    public var MediaController: MediaControllerClass { .init(parent: self, name: "MediaController") }
+}
 
-// class MediaController: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.MediaController], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class MediaController: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
