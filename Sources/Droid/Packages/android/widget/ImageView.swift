9e4b2585b14e315366d31a447a5d60d84b2d5b06
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 07:35:44 +0400

    🪚 ImageView: initial implementation

--- Sources/Droid/Packages/android/widget/ImageView.swift
+++ Sources/Droid/Packages/android/widget/ImageView.swift
@@ -1,22 +1,18 @@
-// //
-// //  ImageView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ImageView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ImageViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var ImageView: ImageViewClass { .init(parent: self, name: "ImageView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ImageViewClass: JClassName, @unchecked Sendable {}
+    public var ImageView: ImageViewClass { .init(parent: self, name: "ImageView") }
+}
 
-// class ImageView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ImageView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ImageView: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
