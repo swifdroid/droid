20bf60fe215893bd2efdc8cc9907d463d1838918
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 06:41:40 +0400

    🪚 DatePicker: initial implementation

--- Sources/Droid/Packages/android/widget/DatePicker.swift
+++ Sources/Droid/Packages/android/widget/DatePicker.swift
@@ -1,22 +1,18 @@
-// //
-// //  DatePicker.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  DatePicker.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class DatePickerClass: JClassName, @unchecked Sendable {}
-    
-//     public var DatePicker: DatePickerClass { .init(parent: self, name: "DatePicker") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class DatePickerClass: JClassName, @unchecked Sendable {}
+    public var DatePicker: DatePickerClass { .init(parent: self, name: "DatePicker") }
+}
 
-// class DatePicker: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.DatePicker], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class DatePicker: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
