c5b46fc4e6be5509a19f2869388990da4e515910
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 13:30:27 +0400

    🪚 TextClock: initial implementation

--- Sources/Droid/Packages/android/widget/TextClock.swift
+++ Sources/Droid/Packages/android/widget/TextClock.swift
@@ -1,22 +1,18 @@
-// //
-// //  TextClock.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  TextClock.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class TextClockClass: JClassName, @unchecked Sendable {}
-    
-//     public var TextClock: TextClockClass { .init(parent: self, name: "TextClock") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class TextClockClass: JClassName, @unchecked Sendable {}
+    public var TextClock: TextClockClass { .init(parent: self, name: "TextClock") }
+}
 
-// class TextClock: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.TextClock], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class TextClock: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
