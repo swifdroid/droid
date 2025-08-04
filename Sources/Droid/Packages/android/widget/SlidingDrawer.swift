1432eaddc2455af8c83cd72f9e8a09be7bc0de6d
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 09:06:37 +0400

    🪚 SlidingDrawer: initial implementation

--- Sources/Droid/Packages/android/widget/SlidingDrawer.swift
+++ Sources/Droid/Packages/android/widget/SlidingDrawer.swift
@@ -1,22 +1,18 @@
-// //
-// //  SlidingDrawer.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  SlidingDrawer.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class SlidingDrawerClass: JClassName, @unchecked Sendable {}
-    
-//     public var SlidingDrawer: SlidingDrawerClass { .init(parent: self, name: "SlidingDrawer") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class SlidingDrawerClass: JClassName, @unchecked Sendable {}
+    public var SlidingDrawer: SlidingDrawerClass { .init(parent: self, name: "SlidingDrawer") }
+}
 
-// class SlidingDrawer: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.SlidingDrawer], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class SlidingDrawer: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
