806e3f187405b9cdc74c2606c829f079e59e800e
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 08:51:53 +0400

    🪚 RatingBar: initial implementation

--- Sources/Droid/Packages/android/widget/RatingBar.swift
+++ Sources/Droid/Packages/android/widget/RatingBar.swift
@@ -1,22 +1,18 @@
-// //
-// //  RatingBar.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  RatingBar.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class RatingBarClass: JClassName, @unchecked Sendable {}
-    
-//     public var RatingBar: RatingBarClass { .init(parent: self, name: "RatingBar") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class RatingBarClass: JClassName, @unchecked Sendable {}
+    public var RatingBar: RatingBarClass { .init(parent: self, name: "RatingBar") }
+}
 
-// class RatingBar: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.RatingBar], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class RatingBar: AbsSeekBar, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
