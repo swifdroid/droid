4181dfb80e06e43d59bfeb9536f385545fc94b23
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 09:06:13 +0400

    🪚 ScrollView: initial implementation

--- Sources/Droid/Packages/android/widget/ScrollView.swift
+++ Sources/Droid/Packages/android/widget/ScrollView.swift
@@ -1,22 +1,18 @@
-// //
-// //  ScrollView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ScrollView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ScrollViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var ScrollView: ScrollViewClass { .init(parent: self, name: "ScrollView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ScrollViewClass: JClassName, @unchecked Sendable {}
+    public var ScrollView: ScrollViewClass { .init(parent: self, name: "ScrollView") }
+}
 
-// class ScrollView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ScrollView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ScrollView: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
