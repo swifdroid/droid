e8ba3b28dd8871e0b69c61f91e3e68231e80bade
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 06:16:17 +0400

    🪚 Implement `Checkable` protocol

---
+++ Sources/Droid/Packages/android/widget/Checkable.swift
@@ -0,0 +1,33 @@
+//
+//  Checkable.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 05.08.2025.
+//
+
+#if canImport(AndroidLooper)
+import AndroidLooper
+#endif
+
+#if canImport(AndroidLooper)
+@UIThreadActor
+#endif
+public protocol Checkable: ViewInstanceable {
+    func setChecked(_ checked: Bool)
+    func isChecked() -> Bool
+    func toggle()
+}
+
+extension Checkable {
+    public func setChecked(_ checked: Bool) {
+        instance?.callVoidMethod(name: "setChecked", args: [checked])
+    }
+
+    public func isChecked() -> Bool {
+        instance?.callBoolMethod(name: "isChecked") ?? false
+    }
+
+    public func toggle() {
+        instance?.callVoidMethod(name: "toggle")
+    }
+}
