f1fa40f0bb1989b406fdf551b88356c8ccd2b87f
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 05:01:06 +0400

    🪚 AutoCompleteTextView: initial implementation

--- Sources/Droid/Packages/android/widget/AutoCompleteTextView.swift
+++ Sources/Droid/Packages/android/widget/AutoCompleteTextView.swift
@@ -1,22 +1,21 @@
-// //
-// //  AutoCompleteTextView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  AutoCompleteTextView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class AutoCompleteTextViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var AutoCompleteTextView: AutoCompleteTextViewClass { .init(parent: self, name: "AutoCompleteTextView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class AutoCompleteTextViewClass: JClassName, @unchecked Sendable {}
+    public var AutoCompleteTextView: AutoCompleteTextViewClass { .init(parent: self, name: "AutoCompleteTextView") }
+}
 
-// class AutoCompleteTextView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.AutoCompleteTextView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class AutoCompleteTextView: EditText, @unchecked Sendable {
+    /// The JNI class name
+    open override class var className: JClassName { .android.widget.AutoCompleteTextView }
+
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
