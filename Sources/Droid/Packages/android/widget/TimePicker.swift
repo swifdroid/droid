5201c931333ba329201d80e05e93456e689466d8
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 13:30:43 +0400

    🪚 TimePicker: initial implementation

--- Sources/Droid/Packages/android/widget/TimePicker.swift
+++ Sources/Droid/Packages/android/widget/TimePicker.swift
@@ -1,22 +1,18 @@
-// //
-// //  TimePicker.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  TimePicker.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class TimePickerClass: JClassName, @unchecked Sendable {}
-    
-//     public var TimePicker: TimePickerClass { .init(parent: self, name: "TimePicker") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class TimePickerClass: JClassName, @unchecked Sendable {}
+    public var TimePicker: TimePickerClass { .init(parent: self, name: "TimePicker") }
+}
 
-// class TimePicker: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.TimePicker], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class TimePicker: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
