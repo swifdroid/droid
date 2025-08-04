b68f1824f3e584b4487d6bd40043b7a1fbe51923
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 06:16:00 +0400

    🪚 Chronometer: initial implementation

--- Sources/Droid/Packages/android/widget/Chronometer.swift
+++ Sources/Droid/Packages/android/widget/Chronometer.swift
@@ -1,22 +1,18 @@
-// //
-// //  Chronometer.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  Chronometer.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ChronometerClass: JClassName, @unchecked Sendable {}
-    
-//     public var Chronometer: ChronometerClass { .init(parent: self, name: "Chronometer") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ChronometerClass: JClassName, @unchecked Sendable {}
+    public var Chronometer: ChronometerClass { .init(parent: self, name: "Chronometer") }
+}
 
-// class Chronometer: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.Chronometer], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class Chronometer: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
