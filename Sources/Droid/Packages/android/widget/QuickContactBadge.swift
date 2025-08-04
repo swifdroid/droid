260e6265cc4c691b883549b2295cf00b9918c366
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 07:36:24 +0400

    🪚 QuickContactBadge: initial implementation

--- Sources/Droid/Packages/android/widget/QuickContactBadge.swift
+++ Sources/Droid/Packages/android/widget/QuickContactBadge.swift
@@ -1,22 +1,18 @@
-// //
-// //  QuickContactBadge.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  QuickContactBadge.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class QuickContactBadgeClass: JClassName, @unchecked Sendable {}
-    
-//     public var QuickContactBadge: QuickContactBadgeClass { .init(parent: self, name: "QuickContactBadge") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class QuickContactBadgeClass: JClassName, @unchecked Sendable {}
+    public var QuickContactBadge: QuickContactBadgeClass { .init(parent: self, name: "QuickContactBadge") }
+}
 
-// class QuickContactBadge: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.QuickContactBadge], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class QuickContactBadge: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
