652b63449b059d17f6ef3adbd98aba485b5a66df
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 06:16:26 +0400

    🪚 CheckBox: initial implementation

--- Sources/Droid/Packages/android/widget/CheckBox.swift
+++ Sources/Droid/Packages/android/widget/CheckBox.swift
@@ -1,22 +1,18 @@
-// //
-// //  CheckBox.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  CheckBox.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class CheckBoxClass: JClassName, @unchecked Sendable {}
-    
-//     public var CheckBox: CheckBoxClass { .init(parent: self, name: "CheckBox") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class CheckBoxClass: JClassName, @unchecked Sendable {}
+    public var CheckBox: CheckBoxClass { .init(parent: self, name: "CheckBox") }
+}
 
-// class CheckBox: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.CheckBox], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class CheckBox: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
