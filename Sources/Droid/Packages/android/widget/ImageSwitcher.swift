a197f9f7de042b7a675b5377a54ac5ecad67da24
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 07:23:33 +0400

    🪚 ImageSwitcher: initial implementation

--- Sources/Droid/Packages/android/widget/ImageSwitcher.swift
+++ Sources/Droid/Packages/android/widget/ImageSwitcher.swift
@@ -1,22 +1,18 @@
-// //
-// //  ImageSwitcher.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ImageSwitcher.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ImageSwitcherClass: JClassName, @unchecked Sendable {}
-    
-//     public var ImageSwitcher: ImageSwitcherClass { .init(parent: self, name: "ImageSwitcher") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ImageSwitcherClass: JClassName, @unchecked Sendable {}
+    public var ImageSwitcher: ImageSwitcherClass { .init(parent: self, name: "ImageSwitcher") }
+}
 
-// class ImageSwitcher: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ImageSwitcher], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ImageSwitcher: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
