46a82399a5f818db261d7f0304c655d897664531
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 06:42:12 +0400

    🪚 Gallery: initial implementation

--- Sources/Droid/Packages/android/widget/Gallery.swift
+++ Sources/Droid/Packages/android/widget/Gallery.swift
@@ -1,22 +1,34 @@
-// //
-// //  Gallery.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  Gallery.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class GalleryClass: JClassName, @unchecked Sendable {}
-    
-//     public var Gallery: GalleryClass { .init(parent: self, name: "Gallery") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class GalleryClass: JClassName, @unchecked Sendable {}    
+    public var Gallery: GalleryClass { .init(parent: self, name: "Gallery") }
+}
+extension AndroidPackage.WidgetPackage.GalleryClass {
+    public class LayoutParamsClass: JClassName, @unchecked Sendable {}
+    public var LayoutParams: LayoutParamsClass { .init(parent: self, name: "LayoutParams", isInnerClass: true) }
+}
+extension LayoutParams.Class {
+    static let gallery: Self = .init(.android.widget.Gallery.LayoutParams)
+}
 
-// class Gallery: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.Gallery], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class Gallery: View, @unchecked Sendable {
+    /// The JNI class name
+    open override class var className: JClassName { .android.widget.Gallery }
+    open override class var layoutParamsClass: LayoutParams.Class { .gallery }
+
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+
+    @discardableResult
+    public override init (@BodyBuilder content: BodyBuilder.SingleView) {
+        super.init(content: content)
+    }
+}
