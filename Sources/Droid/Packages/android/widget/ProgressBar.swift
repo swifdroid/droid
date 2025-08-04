f0b08f3aabfc7f820ba67a1e1a31565f420aa26e
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 05:01:54 +0400

    🪚 ProgressBar: initial implementation

--- Sources/Droid/Packages/android/widget/ProgressBar.swift
+++ Sources/Droid/Packages/android/widget/ProgressBar.swift
@@ -1,22 +1,21 @@
-// //
-// //  ProgressBar.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ProgressBar.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ProgressBarClass: JClassName, @unchecked Sendable {}
-    
-//     public var ProgressBar: ProgressBarClass { .init(parent: self, name: "ProgressBar") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ProgressBarClass: JClassName, @unchecked Sendable {}    
+    public var ProgressBar: ProgressBarClass { .init(parent: self, name: "ProgressBar") }
+}
 
-// class ProgressBar: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ProgressBar], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ProgressBar: View, @unchecked Sendable {
+    /// The JNI class name
+    public override class var className: JClassName { .android.widget.ProgressBar }
+
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
