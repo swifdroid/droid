77a5c802ab255b6bfd7a187d11d94a5e56eacfcb
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 05:02:45 +0400

    🪚 SeekBar: initial implementation

--- Sources/Droid/Packages/android/widget/SeekBar.swift
+++ Sources/Droid/Packages/android/widget/SeekBar.swift
@@ -1,36 +1,33 @@
-// //
-// //  SeekBar.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  SeekBar.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class SeekBarClass: JClassName, @unchecked Sendable {}
-//     public class SeekBarOnSeekBarChangeListenerInterface: JClassName, @unchecked Sendable {}
+extension AndroidPackage.WidgetPackage {
+    public class SeekBarClass: JClassName, @unchecked Sendable {}
+    public class SeekBarOnSeekBarChangeListenerInterface: JClassName, @unchecked Sendable {}
     
-//     public var SeekBar: SeekBarClass { .init(parent: self, name: "SeekBar") }
-//     public var SeekBarOnSeekBarChangeListener: SeekBarOnSeekBarChangeListenerInterface { .init(parent: self, name: "SeekBar$OnSeekBarChangeListener") }
-// }
+    public var SeekBar: SeekBarClass { .init(parent: self, name: "SeekBar") }
+    public var SeekBarOnSeekBarChangeListener: SeekBarOnSeekBarChangeListenerInterface { .init(parent: self, name: "SeekBar$OnSeekBarChangeListener") }
+}
 
-// class SeekBar: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.SeekBar], args: [.object(.android.content.Context) / context])
-//     }
+open class SeekBar: AbsSeekBar, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
     
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
+    public func setOnSeekBarChangeListener(_ listener: JClass) {
+        // callVoidWithMethod("setOnSeekBarChangeListener", .object(.android.widget.SeekBarOnSeekBarChangeListener) / listener)
+    }
     
-//     public func setOnSeekBarChangeListener(_ listener: JClass) {
-//         callVoidWithMethod("setOnSeekBarChangeListener", .object(.android.widget.SeekBarOnSeekBarChangeListener) / listener)
-//     }
+    public func setProgress(_ value: Int) {
+        // callVoidWithMethod("setProgress", .int(value))
+    }
     
-//     public func setProgress(_ value: Int) {
-//         callVoidWithMethod("setProgress", .int(value))
-//     }
-    
-//     public func setProgress(_ value: Int, animated: Bool) {
-//         callVoidWithMethod("setProgress", .int(value), .boolean(animated))
-//     }
-// }
+    public func setProgress(_ value: Int, animated: Bool) {
+        // callVoidWithMethod("setProgress", .int(value), .boolean(animated))
+    }
+}
