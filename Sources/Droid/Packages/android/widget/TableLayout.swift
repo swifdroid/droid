3b32095b43cc39e7d651533878b38fff3cf59332
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 05:03:18 +0400

    🪚 TableLayout: initial implementation

--- Sources/Droid/Packages/android/widget/TableLayout.swift
+++ Sources/Droid/Packages/android/widget/TableLayout.swift
@@ -1,22 +1,29 @@
-// //
-// //  TableLayout.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  TableLayout.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class TableLayoutClass: JClassName, @unchecked Sendable {}
-    
-//     public var TableLayout: TableLayoutClass { .init(parent: self, name: "TableLayout") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class TableLayoutClass: JClassName, @unchecked Sendable {}
+    public var TableLayout: TableLayoutClass { .init(parent: self, name: "TableLayout") }
+}
+extension AndroidPackage.WidgetPackage.TableLayoutClass {
+    public class LayoutParamsClass: JClassName, @unchecked Sendable {}
+    public var LayoutParams: LayoutParamsClass { .init(parent: self, name: "LayoutParams", isInnerClass: true) }
+}
+extension LayoutParams.Class {
+    static let tableLayout: Self = .init(.android.widget.TableLayout.LayoutParams)
+}
 
-// class TableLayout: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.TableLayout], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class TableLayout: View, @unchecked Sendable {
+    /// The JNI class name
+    open override class var className: JClassName { .android.widget.TableLayout }
+    open override class var layoutParamsClass: LayoutParams.Class { .tableLayout }
+
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
