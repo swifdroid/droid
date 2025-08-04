da1288cb872968074ad240c6144b6e0ad8f93243
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 07:36:04 +0400

    🪚 MultiAutoCompleteTextView: initial implementation

--- Sources/Droid/Packages/android/widget/MultiAutoCompleteTextView.swift
+++ Sources/Droid/Packages/android/widget/MultiAutoCompleteTextView.swift
@@ -1,22 +1,18 @@
-// //
-// //  MultiAutoCompleteTextView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  MultiAutoCompleteTextView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class MultiAutoCompleteTextViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var MultiAutoCompleteTextView: MultiAutoCompleteTextViewClass { .init(parent: self, name: "MultiAutoCompleteTextView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class MultiAutoCompleteTextViewClass: JClassName, @unchecked Sendable {}
+    public var MultiAutoCompleteTextView: MultiAutoCompleteTextViewClass { .init(parent: self, name: "MultiAutoCompleteTextView") }
+}
 
-// class MultiAutoCompleteTextView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.MultiAutoCompleteTextView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class MultiAutoCompleteTextView: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
