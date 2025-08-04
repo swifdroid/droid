e0318be7ea5e6d734839254e4a0969d45eb8fd48
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 09:06:24 +0400

    🪚 SearchView: initial implementation

--- Sources/Droid/Packages/android/widget/SearchView.swift
+++ Sources/Droid/Packages/android/widget/SearchView.swift
@@ -1,22 +1,18 @@
-// //
-// //  SearchView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  SearchView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class SearchViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var SearchView: SearchViewClass { .init(parent: self, name: "SearchView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class SearchViewClass: JClassName, @unchecked Sendable {}
+    public var SearchView: SearchViewClass { .init(parent: self, name: "SearchView") }
+}
 
-// class SearchView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.SearchView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class SearchView: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
