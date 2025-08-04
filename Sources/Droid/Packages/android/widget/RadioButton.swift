b8eb646ef0209c304c9bee4458778d2db635fe63
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 08:51:33 +0400

    🪚 RadioButton: initial implementation

--- Sources/Droid/Packages/android/widget/RadioButton.swift
+++ Sources/Droid/Packages/android/widget/RadioButton.swift
@@ -1,22 +1,18 @@
-// //
-// //  RadioButton.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  RadioButton.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class RadioButtonClass: JClassName, @unchecked Sendable {}
-    
-//     public var RadioButton: RadioButtonClass { .init(parent: self, name: "RadioButton") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class RadioButtonClass: JClassName, @unchecked Sendable {}
+    public var RadioButton: RadioButtonClass { .init(parent: self, name: "RadioButton") }
+}
 
-// class RadioButton: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.RadioButton], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class RadioButton: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
