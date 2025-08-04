f61c4e49992fc79aa15f254399839a8a44dcc1b1
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 13:30:51 +0400

    🪚 ToggleButton: initial implementation

--- Sources/Droid/Packages/android/widget/ToggleButton.swift
+++ Sources/Droid/Packages/android/widget/ToggleButton.swift
@@ -1,22 +1,18 @@
-// //
-// //  ToggleButton.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ToggleButton.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ToggleButtonClass: JClassName, @unchecked Sendable {}
-    
-//     public var ToggleButton: ToggleButtonClass { .init(parent: self, name: "ToggleButton") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ToggleButtonClass: JClassName, @unchecked Sendable {}
+    public var ToggleButton: ToggleButtonClass { .init(parent: self, name: "ToggleButton") }
+}
 
-// class ToggleButton: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ToggleButton], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ToggleButton: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
