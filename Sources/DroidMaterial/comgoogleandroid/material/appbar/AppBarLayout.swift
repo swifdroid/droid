4b1733e5ab21e33222755186a03383f316dd5e40
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 04:22:34 +0400

    🪚 AppBarLayout: initial implementation

--- Sources/DroidMaterial/comgoogleandroid/material/appbar/AppBarLayout.swift
+++ Sources/DroidMaterial/comgoogleandroid/material/appbar/AppBarLayout.swift
@@ -9,19 +9,31 @@ import Droid
 
 extension ComGoogleAndroidPackage.MaterialPackage.AppBarPackage {
     public class AppBarLayoutClass: JClassName, @unchecked Sendable {}
-    
     public var AppBarLayout: AppBarLayoutClass { .init(parent: self, name: "AppBarLayout") }
 }
+extension ComGoogleAndroidPackage.MaterialPackage.AppBarPackage.AppBarLayoutClass {
+    public class LayoutParamsClass: JClassName, @unchecked Sendable {}
+    public var LayoutParams: LayoutParamsClass { .init(parent: self, name: "LayoutParams", isInnerClass: true) }
+}
+extension LayoutParams.Class {
+    static let materialAppBarLayout: Self = .init(.comGoogleAndroid.material.appbar.AppBarLayout.LayoutParams)
+}
+
+open class AppBarLayout: ViewGroup, @unchecked Sendable {
+    /// The JNI class name
+    open override class var className: JClassName { .comGoogleAndroid.material.appbar.AppBarLayout }
+    open override class var layoutParamsClass: LayoutParams.Class { .materialAppBarLayout }
     open override class var gradleDependencies: [String] { [
         "implementation(\"com.google.android.material:material:1.12.0\")"
     ] }
 
-// class AppBarLayout: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.comGoogleAndroid.material.appbar.AppBarLayout], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+
+    @discardableResult
+    public override init (@BodyBuilder content: BodyBuilder.SingleView) {
+        super.init(content: content)
+    }
+}
