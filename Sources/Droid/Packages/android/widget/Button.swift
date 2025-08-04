8c335373b9da7ff619e3c9de00129cde3c9bc658
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 05:01:22 +0400

    🪚 Button: initial implementation

--- Sources/Droid/Packages/android/widget/Button.swift
+++ Sources/Droid/Packages/android/widget/Button.swift
@@ -1,35 +1,25 @@
-// //
-// //  Button.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  Button.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ButtonClass: JClassName, @unchecked Sendable {}
-    
-//     public var Button: ButtonClass { .init(parent: self, name: "Button") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ButtonClass: JClassName, @unchecked Sendable {}
+    public var Button: ButtonClass { .init(parent: self, name: "Button") }
+}
 
-// class Button: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.Button], args: [.object(.android.content.Context) / context])
-//     }
+open class Button: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
     
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-    
-//     func setText(_ text: String) {
-//         text.withCString { cstr in
-//             let jsss = environment.pointer.pointee?.pointee.NewStringUTF(environment.pointer, cstr)
-//             callVoidWithMethod("setText", .object(.java.lang.CharSequence) / JStringWrapper.init(v: jsss!))
-//         }
-//     }
-// }
-
-// struct JStringWrapper: JValuable {
-//     let v: jstring
-    
-//     var jValue: jvalue { .init(l: v) }
-// }
+    public func setText(_ text: String) {
+        // text.withCString { cstr in
+        //     let jsss = environment.pointer.pointee?.pointee.NewStringUTF(environment.pointer, cstr)
+        //     callVoidWithMethod("setText", .object(.java.lang.CharSequence) / JStringWrapper.init(v: jsss!))
+        // }
+    }
+}
