b8c0802f0be8c6b21724234b4699e4a61763c7db
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 06:41:59 +0400

    🪚 ExpandableListView: initial implementation

--- Sources/Droid/Packages/android/widget/ExpandableListView.swift
+++ Sources/Droid/Packages/android/widget/ExpandableListView.swift
@@ -1,22 +1,18 @@
-// //
-// //  ExpandableListView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ExpandableListView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ExpandableListViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var ExpandableListView: ExpandableListViewClass { .init(parent: self, name: "ExpandableListView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ExpandableListViewClass: JClassName, @unchecked Sendable {}
+    public var ExpandableListView: ExpandableListViewClass { .init(parent: self, name: "ExpandableListView") }
+}
 
-// class ExpandableListView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ExpandableListView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ExpandableListView: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
