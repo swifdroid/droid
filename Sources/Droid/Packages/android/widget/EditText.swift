572ac0077b93de7fe04a4b9b3fb8907538df3b80
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 05:00:46 +0400

    🪚 EditText: initial implementation

--- Sources/Droid/Packages/android/widget/EditText.swift
+++ Sources/Droid/Packages/android/widget/EditText.swift
@@ -1,26 +1,25 @@
-// //
-// //  EditText.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  EditText.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class EditTextClass: JClassName, @unchecked Sendable {}
-    
-//     public var EditText: EditTextClass { .init(parent: self, name: "EditText") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class EditTextClass: JClassName, @unchecked Sendable {}
+    public var EditText: EditTextClass { .init(parent: self, name: "EditText") }
+}
 
-// class EditText: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.EditText], args: [.object(.android.content.Context) / context])
-//     }
+open class EditText: TextView, @unchecked Sendable {
+    /// The JNI class name
+    open override class var className: JClassName { .android.widget.EditText }
+
+    @discardableResult
+    public override init() {
+        super.init()
+    }
     
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-    
-//     func addTextChangedListener(_ textWatcher: JClass) {
-//         callVoidWithMethod("addTextChangedListener", .object(.android.text.TextWatcher) / textWatcher)
-//     }
-// }
+    func addTextChangedListener(_ textWatcher: JClass) {
+        // callVoidWithMethod("addTextChangedListener", .object(.android.text.TextWatcher) / textWatcher)
+    }
+}
