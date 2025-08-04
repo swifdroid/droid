d55c0e0a3c734c6b9e1a5af4547faa4dd78c892b
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 07:01:21 +0400

    🪚 ImageButton: initial implementation

--- Sources/Droid/Packages/android/widget/ImageButton.swift
+++ Sources/Droid/Packages/android/widget/ImageButton.swift
@@ -1,22 +1,18 @@
-// //
-// //  ImageButton.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ImageButton.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ImageButtonClass: JClassName, @unchecked Sendable {}
-    
-//     public var ImageButton: ImageButtonClass { .init(parent: self, name: "ImageButton") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ImageButtonClass: JClassName, @unchecked Sendable {}
+    public var ImageButton: ImageButtonClass { .init(parent: self, name: "ImageButton") }
+}
 
-// class ImageButton: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ImageButton], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ImageButton: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
