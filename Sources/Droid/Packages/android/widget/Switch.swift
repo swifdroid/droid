40cd52bd29eb72bdb371b9af2778b830fe67c561
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 05:03:05 +0400

    🪚 Switch: initial implementation

--- Sources/Droid/Packages/android/widget/Switch.swift
+++ Sources/Droid/Packages/android/widget/Switch.swift
@@ -1,22 +1,18 @@
-// //
-// //  Switch.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  Switch.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class SwitchClass: JClassName, @unchecked Sendable {}
-    
-//     public var Switch: SwitchClass { .init(parent: self, name: "Switch") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class SwitchClass: JClassName, @unchecked Sendable {}
+    public var Switch: SwitchClass { .init(parent: self, name: "Switch") }
+}
 
-// class Switch: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.Switch], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class Switch: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
