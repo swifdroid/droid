d04cc48994ee720962b4fad44a0039fe6f7313a3
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 07:01:29 +0400

    🪚 ListView: initial implementation

--- Sources/Droid/Packages/android/widget/ListView.swift
+++ Sources/Droid/Packages/android/widget/ListView.swift
@@ -1,22 +1,18 @@
-// //
-// //  ListView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ListView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ListViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var ListView: ListViewClass { .init(parent: self, name: "ListView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ListViewClass: JClassName, @unchecked Sendable {}
+    public var ListView: ListViewClass { .init(parent: self, name: "ListView") }
+}
 
-// class ListView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ListView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ListView: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
