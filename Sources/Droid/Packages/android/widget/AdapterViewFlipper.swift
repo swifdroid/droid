568944d13c5854de361c92eb020efc84482be7da
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-04 07:23:11 +0400

    🪚 AdapterViewFlipper: initial implementation

--- Sources/Droid/Packages/android/widget/AdapterViewFlipper.swift
+++ Sources/Droid/Packages/android/widget/AdapterViewFlipper.swift
@@ -1,22 +1,26 @@
-// //
-// //  AdapterViewFlipper.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  AdapterViewFlipper.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class AdapterViewFlipperClass: JClassName, @unchecked Sendable {}
-    
-//     public var AdapterViewFlipper: AdapterViewFlipperClass { .init(parent: self, name: "AdapterViewFlipper") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class AdapterViewFlipperClass: JClassName, @unchecked Sendable {}
+    public var AdapterViewFlipper: AdapterViewFlipperClass { .init(parent: self, name: "AdapterViewFlipper") }
+}
 
-// class AdapterViewFlipper: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.AdapterViewFlipper], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class AdapterViewFlipper: View, @unchecked Sendable {//: AdapterViewAnimator {
+    /// The JNI class name
+    open override class var className: JClassName { .android.widget.AdapterViewFlipper }
+
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+
+    @discardableResult
+    public override init (@BodyBuilder content: BodyBuilder.SingleView) {
+        super.init(content: content)
+    }
+}
