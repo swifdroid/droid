6900d770aed76a28fcee70c9455f9553500a9148
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-05 05:05:48 +0400

    🪚 TwoLineListItem: initial implementation

--- Sources/Droid/Packages/android/widget/TwoLineListItem.swift
+++ Sources/Droid/Packages/android/widget/TwoLineListItem.swift
@@ -1,22 +1,18 @@
-// //
-// //  TwoLineListItem.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  TwoLineListItem.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class TwoLineListItemClass: JClassName, @unchecked Sendable {}
-    
-//     public var TwoLineListItem: TwoLineListItemClass { .init(parent: self, name: "TwoLineListItem") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class TwoLineListItemClass: JClassName, @unchecked Sendable {}
+    public var TwoLineListItem: TwoLineListItemClass { .init(parent: self, name: "TwoLineListItem") }
+}
 
-// class TwoLineListItem: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.TwoLineListItem], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class TwoLineListItem: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
