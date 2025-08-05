f7f46f1a674dc49d9942792c0e51cc559c9147ed
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-05 05:06:10 +0400

    🪚 ViewFlipper: initial implementation

--- Sources/Droid/Packages/android/widget/ViewFlipper.swift
+++ Sources/Droid/Packages/android/widget/ViewFlipper.swift
@@ -1,22 +1,18 @@
-// //
-// //  ViewFlipper.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ViewFlipper.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ViewFlipperClass: JClassName, @unchecked Sendable {}
-    
-//     public var ViewFlipper: ViewFlipperClass { .init(parent: self, name: "ViewFlipper") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ViewFlipperClass: JClassName, @unchecked Sendable {}
+    public var ViewFlipper: ViewFlipperClass { .init(parent: self, name: "ViewFlipper") }
+}
 
-// class ViewFlipper: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ViewFlipper], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ViewFlipper: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
