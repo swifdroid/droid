18c6870e0716a375b1bf7bb188697a70df7a1ddd
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-05 05:06:30 +0400

    🪚 ZoomControls: initial implementation

--- Sources/Droid/Packages/android/widget/ZoomControls.swift
+++ Sources/Droid/Packages/android/widget/ZoomControls.swift
@@ -1,22 +1,18 @@
-// //
-// //  ZoomControls.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ZoomControls.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ZoomControlsClass: JClassName, @unchecked Sendable {}
-    
-//     public var ZoomControls: ZoomControlsClass { .init(parent: self, name: "ZoomControls") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ZoomControlsClass: JClassName, @unchecked Sendable {}
+    public var ZoomControls: ZoomControlsClass { .init(parent: self, name: "ZoomControls") }
+}
 
-// class ZoomControls: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ZoomControls], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ZoomControls: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
