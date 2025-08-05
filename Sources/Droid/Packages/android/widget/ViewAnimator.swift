a55edf45be18e773912ab402ff6d6dda0788dd7f
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-05 05:06:04 +0400

    🪚 ViewAnimator: initial implementation

--- Sources/Droid/Packages/android/widget/ViewAnimator.swift
+++ Sources/Droid/Packages/android/widget/ViewAnimator.swift
@@ -1,22 +1,18 @@
-// //
-// //  ViewAnimator.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ViewAnimator.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ViewAnimatorClass: JClassName, @unchecked Sendable {}
-    
-//     public var ViewAnimator: ViewAnimatorClass { .init(parent: self, name: "ViewAnimator") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ViewAnimatorClass: JClassName, @unchecked Sendable {}
+    public var ViewAnimator: ViewAnimatorClass { .init(parent: self, name: "ViewAnimator") }
+}
 
-// class ViewAnimator: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ViewAnimator], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ViewAnimator: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
