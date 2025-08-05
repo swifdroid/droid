70219230ea60ca277403b844319c3169dc2db1ed
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-05 05:05:20 +0400

    🪚 Toolbar: initial implementation

--- Sources/Droid/Packages/android/widget/Toolbar.swift
+++ Sources/Droid/Packages/android/widget/Toolbar.swift
@@ -1,22 +1,18 @@
-// //
-// //  Toolbar.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  Toolbar.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ToolbarClass: JClassName, @unchecked Sendable {}
-    
-//     public var Toolbar: ToolbarClass { .init(parent: self, name: "Toolbar") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ToolbarClass: JClassName, @unchecked Sendable {}
+    public var Toolbar: ToolbarClass { .init(parent: self, name: "Toolbar") }
+}
 
-// class Toolbar: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.Toolbar], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class Toolbar1: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
