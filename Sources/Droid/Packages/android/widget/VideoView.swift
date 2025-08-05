caff8647c9e1e153ca70f4439fd698dbd17579e6
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-05 05:05:56 +0400

    🪚 VideoView: initial implementation

--- Sources/Droid/Packages/android/widget/VideoView.swift
+++ Sources/Droid/Packages/android/widget/VideoView.swift
@@ -1,22 +1,18 @@
-// //
-// //  VideoView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  VideoView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class VideoViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var VideoView: VideoViewClass { .init(parent: self, name: "VideoView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class VideoViewClass: JClassName, @unchecked Sendable {}
+    public var VideoView: VideoViewClass { .init(parent: self, name: "VideoView") }
+}
 
-// class VideoView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.VideoView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class VideoView: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
