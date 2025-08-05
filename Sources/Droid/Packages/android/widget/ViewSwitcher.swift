000e796d5489ab2785fa54963898023d8e9492ad
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-05 05:06:16 +0400

    🪚 ViewSwitcher: initial implementation

--- Sources/Droid/Packages/android/widget/ViewSwitcher.swift
+++ Sources/Droid/Packages/android/widget/ViewSwitcher.swift
@@ -1,22 +1,18 @@
-// //
-// //  ViewSwitcher.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ViewSwitcher.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ViewSwitcherClass: JClassName, @unchecked Sendable {}
-    
-//     public var ViewSwitcher: ViewSwitcherClass { .init(parent: self, name: "ViewSwitcher") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ViewSwitcherClass: JClassName, @unchecked Sendable {}
+    public var ViewSwitcher: ViewSwitcherClass { .init(parent: self, name: "ViewSwitcher") }
+}
 
-// class ViewSwitcher: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ViewSwitcher], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ViewSwitcher: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
