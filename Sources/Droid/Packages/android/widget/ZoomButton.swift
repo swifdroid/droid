fba9ee7bb0c6921b067ac064abb795570e4dafa1
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-05 05:06:23 +0400

    🪚 ZoomButton: initial implementation

--- Sources/Droid/Packages/android/widget/ZoomButton.swift
+++ Sources/Droid/Packages/android/widget/ZoomButton.swift
@@ -1,22 +1,18 @@
-// //
-// //  ZoomButton.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ZoomButton.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ZoomButtonClass: JClassName, @unchecked Sendable {}
-    
-//     public var ZoomButton: ZoomButtonClass { .init(parent: self, name: "ZoomButton") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ZoomButtonClass: JClassName, @unchecked Sendable {}
+    public var ZoomButton: ZoomButtonClass { .init(parent: self, name: "ZoomButton") }
+}
 
-// class ZoomButton: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ZoomButton], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ZoomButton: View, @unchecked Sendable {
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+}
