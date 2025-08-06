4df9d621f5f3a40cb9f90229cbefe1f7992af9f3
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-07 01:26:02 +0400

    🪚 ActionMenuView: initial implementation

--- Sources/Droid/Packages/android/widget/ActionMenuView.swift
+++ Sources/Droid/Packages/android/widget/ActionMenuView.swift
@@ -1,22 +1,34 @@
-// //
-// //  ActionMenuView.swift
-// //  Droid
-// //
-// //  Created by Mihael Isaev on 16.01.2022.
-// //
+//
+//  ActionMenuView.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 16.01.2022.
+//
 
-// extension AndroidPackage.WidgetPackage {
-//     public class ActionMenuViewClass: JClassName, @unchecked Sendable {}
-    
-//     public var ActionMenuView: ActionMenuViewClass { .init(parent: self, name: "ActionMenuView") }
-// }
+extension AndroidPackage.WidgetPackage {
+    public class ActionMenuViewClass: JClassName, @unchecked Sendable {}
+    public var ActionMenuView: ActionMenuViewClass { .init(parent: self, name: "ActionMenuView") }
+}
+extension AndroidPackage.WidgetPackage.ActionMenuViewClass {
+    public class LayoutParamsClass: JClassName, @unchecked Sendable {}
+    public var LayoutParams: LayoutParamsClass { .init(parent: self, name: "LayoutParams", isInnerClass: true) }
+}
+extension LayoutParams.Class {
+    static let actionMenuView: Self = .init(.android.widget.ActionMenuView.LayoutParams)
+}
 
-// class ActionMenuView: View {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.android.widget.ActionMenuView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+open class ActionMenuView: LinearLayout, @unchecked Sendable {
+    /// The JNI class name
+    open override class var className: JClassName { .android.widget.ActionMenuView }
+    open override class var layoutParamsClass: LayoutParams.Class { .actionMenuView }
+
+    @discardableResult
+    public override init() {
+        super.init()
+    }
+
+    @discardableResult
+    public override init (@BodyBuilder content: BodyBuilder.SingleView) {
+        super.init(content: content)
+    }
+}
