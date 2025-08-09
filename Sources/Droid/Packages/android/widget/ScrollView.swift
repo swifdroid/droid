fc0bd643a8dda6778ba65fa0a750b48d3edf6a34
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-09 17:45:43 +0400

    🪚 Implement `ScrollView`

--- Sources/Droid/Packages/android/widget/ScrollView.swift
+++ Sources/Droid/Packages/android/widget/ScrollView.swift
@@ -10,9 +10,18 @@ extension AndroidPackage.WidgetPackage {
     public var ScrollView: ScrollViewClass { .init(parent: self, name: "ScrollView") }
 }
 
-open class ScrollView: View, @unchecked Sendable {
+open class ScrollView: ViewGroup, @unchecked Sendable {
+    /// The JNI class name
+    open override class var className: JClassName { .android.widget.ScrollView }
+    public override class var layoutParamsClass: LayoutParams.Class { .frameLayout }
+
     @discardableResult
     public override init() {
         super.init()
+    }
+
+    @discardableResult
+    public override init (@BodyBuilder content: BodyBuilder.SingleView) {
+        super.init(content: content)
     }
 }
