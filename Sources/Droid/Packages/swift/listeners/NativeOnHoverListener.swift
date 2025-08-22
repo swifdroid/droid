9ef7ac208a730c69f5724c186354eb922a83401c
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-22 05:08:12 +0400

    🪚 Implement `NativeOnHoverListener`

---
+++ Sources/Droid/Packages/swift/listeners/NativeOnHoverListener.swift
@@ -0,0 +1,133 @@
+//
+//  NativeOnHoverListener.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 21.08.2025.
+//
+
+#if os(Android)
+import Android
+#if canImport(AndroidLooper)
+import AndroidLooper
+#endif
+
+extension AppKitPackage.ListenersPackage {
+    public class OnHoverListenerClass: JClassName, @unchecked Sendable {}
+    public var OnHoverListener: OnHoverListenerClass { .init(parent: self, name: "NativeOnHoverListener") }
+}
+
+final class NativeOnHoverListener: NativeListener, AnyNativeListener, @unchecked Sendable {
+    /// The JNI class name
+    class var className: JClassName { "stream/swift/droid/appkit/listeners/NativeOnHoverListener" }
+
+    var shouldInitWithViewId: Bool { true }
+
+    typealias Handler = (@UIThreadActor () -> Bool)
+    typealias HandlerWithEvent = (@UIThreadActor (NativeOnHoverListenerEvent) -> Bool)
+
+    /// View
+    var view: View?
+
+    /// Handlers
+    var handler: Handler?
+    var handlerWithEvent: HandlerWithEvent?
+
+    func setHandler(_ view: View?, _ handler: @escaping Handler) -> Self {
+        self.view = view
+        self.handler = handler
+        return self
+    }
+
+    func setHandler(_ view: View?, _ handler: @escaping HandlerWithEvent) -> Self {
+        self.view = view
+        self.handlerWithEvent = handler
+        return self
+    }
+
+    #if canImport(AndroidLooper)
+    @UIThreadActor
+    func handle(_ isSameView: Bool, _ triggerView: NativeListenerTriggerView?, _ motionEvent: MotionEvent?) -> Bool {
+        if let view {
+            return handlerWithEvent?(.init(view, isSameView, triggerView, motionEvent)) ?? handler?() ?? false
+        } else {
+            return handler?() ?? false
+        }
+    }
+    #endif
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnHoverListener_onHover")
+public func nativeListenerOnHover(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint) -> jboolean {
+    guard
+        let listener: NativeOnHoverListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(true, nil, nil)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnHoverListener_onHoverView")
+public func nativeListenerOnHoverView(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, sameView: jboolean, vId: jint, v: jobject) -> jboolean {
+    guard
+        let listener: NativeOnHoverListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let bool = sameView == 1 ? true : false
+    var triggerView: NativeListenerTriggerView?
+    if let object = v.box(JEnv(env))?.object() {
+        triggerView = .init(id: vId, object: object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(bool, triggerView, nil)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnHoverListener_onHoverEvent")
+public func nativeListenerOnHoverEvent(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, event: jobject) -> jboolean {
+    guard
+        let listener: NativeOnHoverListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    var motionEvent: MotionEvent?
+    if let object = event.box(JEnv(env))?.object() {
+        motionEvent = .init(object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(true, nil, motionEvent)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnHoverListener_onHoverViewEvent")
+public func nativeListenerOnHoverViewEvent(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, sameView: jboolean, vId: jint, v: jobject, event: jobject) -> jboolean {
+    guard
+        let listener: NativeOnHoverListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let bool = sameView == 1 ? true : false
+    var triggerView: NativeListenerTriggerView?
+    if let object = v.box(JEnv(env))?.object() {
+        triggerView = .init(id: vId, object: object)
+    }
+    var motionEvent: MotionEvent?
+    if let object = event.box(JEnv(env))?.object() {
+        motionEvent = .init(object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(bool, triggerView, motionEvent)
+    }
+    return result ? 1 : 0
+}
+#endif
+
+public final class NativeOnHoverListenerEvent {
+    public let view: View
+    public let isSameView: Bool
+    public let triggerView: NativeListenerTriggerView?
+    public let motionEvent: MotionEvent?
+    init (_ view: View, _ isSameView: Bool, _ triggerView: NativeListenerTriggerView?, _ motionEvent: MotionEvent?) {
+        self.view = view
+        self.isSameView = isSameView
+        self.triggerView = triggerView
+        self.motionEvent = motionEvent
+    }
+}
--- appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnHoverListener.kt
+++ appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnHoverListener.kt
@@ -3,6 +3,23 @@ package stream.swift.droid.appkit.listeners
 import android.view.MotionEvent
 import android.view.View
 
-    external override fun onHover(v: View?, event: MotionEvent?): Boolean
 class NativeOnHoverListener(private val uniqueId: Int, private val viewId: Int): View.OnHoverListener {
+    override fun onHover(v: View?, event: MotionEvent?): Boolean {
+        if (v != null) {
+            if (event != null) {
+                return onHoverViewEvent(uniqueId, v.id == viewId, v.id, v, event)
+            } else {
+                return onHoverView(uniqueId, v.id == viewId, v.id, v)
+            }
+        } else if (event != null) {
+            return onHoverEvent(uniqueId, event)
+        } else {
+            return onHover(uniqueId)
+        }
+    }
+
+    private external fun onHover(uniqueId: Int): Boolean
+    private external fun onHoverView(uniqueId: Int, sameView: Boolean, vId: Int, v: View): Boolean
+    private external fun onHoverEvent(uniqueId: Int, event: MotionEvent): Boolean
+    private external fun onHoverViewEvent(uniqueId: Int, sameView: Boolean, vId: Int, v: View, event: MotionEvent): Boolean
 }
