b67cf50173ec5e2a24e9c8a1d883c852c259557a
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-22 05:09:58 +0400

    🪚 Implement `NativeOnTouchListener`

---
+++ Sources/Droid/Packages/swift/listeners/NativeOnTouchListener.swift
@@ -0,0 +1,133 @@
+//
+//  NativeOnTouchListener.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 21.08.2025.
+//
+
+#if os(Android)
+import Android
+#if canImport(AndroidLooper)
+import AndroidLooper
+#endif
+
+extension AppKitPackage.ListenersPackage {
+    public class OnTouchListenerClass: JClassName, @unchecked Sendable {}
+    public var OnTouchListener: OnTouchListenerClass { .init(parent: self, name: "NativeOnTouchListener") }
+}
+
+final class NativeOnTouchListener: NativeListener, AnyNativeListener, @unchecked Sendable {
+    /// The JNI class name
+    class var className: JClassName { "stream/swift/droid/appkit/listeners/NativeOnTouchListener" }
+
+    var shouldInitWithViewId: Bool { true }
+
+    typealias Handler = (@UIThreadActor () -> Bool)
+    typealias HandlerWithEvent = (@UIThreadActor (NativeOnTouchListenerEvent) -> Bool)
+
+    /// View
+    var view: View?
+
+    /// Handlers
+    var handler: Handler?
+    var handlerWithEvent: HandlerWithEvent?
+
+    func setHandler(_ view: View?, _ handler: @escaping Handler) -> Self {
+        self.view = view
+        self.handler = handler
+        return self
+    }
+
+    func setHandler(_ view: View?, _ handler: @escaping HandlerWithEvent) -> Self {
+        self.view = view
+        self.handlerWithEvent = handler
+        return self
+    }
+
+    #if canImport(AndroidLooper)
+    @UIThreadActor
+    func handle(_ isSameView: Bool, _ triggerView: NativeListenerTriggerView?, _ motionEvent: MotionEvent?) -> Bool {
+        if let view {
+            return handlerWithEvent?(.init(view, isSameView, triggerView, motionEvent)) ?? handler?() ?? false
+        } else {
+            return handler?() ?? false
+        }
+    }
+    #endif
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnTouchListener_onTouch")
+public func nativeListenerOnTouch(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint) -> jboolean {
+    guard
+        let listener: NativeOnTouchListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(true, nil, nil)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnTouchListener_onTouchView")
+public func nativeListenerOnTouchView(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, sameView: jboolean, vId: jint, v: jobject) -> jboolean {
+    guard
+        let listener: NativeOnTouchListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let bool = sameView == 1 ? true : false
+    var triggerView: NativeListenerTriggerView?
+    if let object = v.box(JEnv(env))?.object() {
+        triggerView = .init(id: vId, object: object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(bool, triggerView, nil)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnTouchListener_onTouchEvent")
+public func nativeListenerOnTouchEvent(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, event: jobject) -> jboolean {
+    guard
+        let listener: NativeOnTouchListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    var motionEvent: MotionEvent?
+    if let object = event.box(JEnv(env))?.object() {
+        motionEvent = .init(object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(true, nil, motionEvent)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnTouchListener_onTouchViewEvent")
+public func nativeListenerOnTouchViewEvent(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, sameView: jboolean, vId: jint, v: jobject, event: jobject) -> jboolean {
+    guard
+        let listener: NativeOnTouchListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let bool = sameView == 1 ? true : false
+    var triggerView: NativeListenerTriggerView?
+    if let object = v.box(JEnv(env))?.object() {
+        triggerView = .init(id: vId, object: object)
+    }
+    var motionEvent: MotionEvent?
+    if let object = event.box(JEnv(env))?.object() {
+        motionEvent = .init(object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(bool, triggerView, motionEvent)
+    }
+    return result ? 1 : 0
+}
+#endif
+
+public final class NativeOnTouchListenerEvent {
+    public let view: View
+    public let isSameView: Bool
+    public let triggerView: NativeListenerTriggerView?
+    public let motionEvent: MotionEvent?
+    init (_ view: View, _ isSameView: Bool, _ triggerView: NativeListenerTriggerView?, _ motionEvent: MotionEvent?) {
+        self.view = view
+        self.isSameView = isSameView
+        self.triggerView = triggerView
+        self.motionEvent = motionEvent
+    }
+}
--- appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnTouchListener.kt
+++ appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnTouchListener.kt
@@ -6,5 +6,22 @@ import android.view.View
 
 class NativeOnTouchListener(private val uniqueId: Int, private val viewId: Int): View.OnTouchListener {
     @SuppressLint("ClickableViewAccessibility")
-    external override fun onTouch(v: View?, event: MotionEvent?): Boolean
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if (v != null) {
+            if (event != null) {
+                return onTouchViewEvent(uniqueId, v.id == viewId, v.id, v, event)
+            } else {
+                return onTouchView(uniqueId, v.id == viewId, v.id, v)
+            }
+        } else if (event != null) {
+            return onTouchEvent(uniqueId, event)
+        } else {
+            return onTouch(uniqueId)
+        }
+    }
+
+    private external fun onTouch(uniqueId: Int): Boolean
+    private external fun onTouchView(uniqueId: Int, sameView: Boolean, vId: Int, v: View): Boolean
+    private external fun onTouchEvent(uniqueId: Int, event: MotionEvent): Boolean
+    private external fun onTouchViewEvent(uniqueId: Int, sameView: Boolean, vId: Int, v: View, event: MotionEvent): Boolean
 }
