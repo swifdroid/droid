1581af5f80094f41ce6ee8ab61acf45d98f8847f
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-22 05:06:34 +0400

    🪚 Implement `NativeOnFocusChangeListener`

---
+++ Sources/Droid/Packages/swift/listeners/NativeOnFocusChangeListener.swift
@@ -0,0 +1,98 @@
+//
+//  NativeOnFocusChangeListener.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 18.08.2025.
+//
+
+#if os(Android)
+import Android
+#if canImport(AndroidLooper)
+import AndroidLooper
+#endif
+
+extension AppKitPackage.ListenersPackage {
+    public class OnFocusChangeListenerClass: JClassName, @unchecked Sendable {}
+    public var OnFocusChangeListener: OnFocusChangeListenerClass { .init(parent: self, name: "NativeOnFocusChangeListener") }
+}
+
+final class NativeOnFocusChangeListener: NativeListener, AnyNativeListener, @unchecked Sendable {
+    /// The JNI class name
+    class var className: JClassName { "stream/swift/droid/appkit/listeners/NativeOnFocusChangeListener" }
+
+    var shouldInitWithViewId: Bool { true }
+
+    typealias Handler = (@UIThreadActor () -> Bool)
+    typealias HandlerWithEvent = (@UIThreadActor (NativeOnFocusChangeListenerEvent) -> Bool)
+
+    /// View
+    var view: View?
+
+    /// Handlers
+    var handler: Handler?
+    var handlerWithEvent: HandlerWithEvent?
+
+    func setHandler(_ view: View?, _ handler: @escaping Handler) -> Self {
+        self.view = view
+        self.handler = handler
+        return self
+    }
+
+    func setHandler(_ view: View?, _ handler: @escaping HandlerWithEvent) -> Self {
+        self.view = view
+        self.handlerWithEvent = handler
+        return self
+    }
+
+    #if canImport(AndroidLooper)
+    @UIThreadActor
+    func handle(_ isSameView: Bool, _ triggerView: NativeListenerTriggerView?, _ hasFocus: Bool) -> Bool {
+        if let view {
+            return handlerWithEvent?(.init(view, isSameView, triggerView, hasFocus)) ?? handler?() ?? false
+        } else {
+            return handler?() ?? false
+        }
+    }
+    #endif
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnFocusChangeListener_onFocusChange")
+public func nativeListenerOnFocusChange(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, hasFocus: jboolean) -> jboolean {
+    guard
+        let listener: NativeOnFocusChangeListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(true, nil, hasFocus == 1)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnFocusChangeListener_onFocusChangeExtended")
+public func nativeListenerOnFocusChangeExtended(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, sameView: jboolean, vId: jint, v: jobject, hasFocus: jboolean) -> jboolean {
+    guard
+        let listener: NativeOnFocusChangeListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let bool = sameView == 1 ? true : false
+    var triggerView: NativeListenerTriggerView?
+    if let object = v.box(JEnv(env))?.object() {
+        triggerView = .init(id: vId, object: object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(bool, triggerView, hasFocus == 1)
+    }
+    return result ? 1 : 0
+}
+#endif
+
+public final class NativeOnFocusChangeListenerEvent {
+    public let view: View
+    public let isSameView: Bool
+    public let triggerView: NativeListenerTriggerView?
+    public let hasFocus: Bool
+    init (_ view: View, _ isSameView: Bool, _ triggerView: NativeListenerTriggerView?, _ hasFocus: Bool) {
+        self.view = view
+        self.isSameView = isSameView
+        self.triggerView = triggerView
+        self.hasFocus = hasFocus
+    }
+}
--- appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnFocusChangeListener.kt
+++ appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnFocusChangeListener.kt
@@ -2,6 +2,15 @@ package stream.swift.droid.appkit.listeners
 
 import android.view.View
 
-    external override fun onFocusChange(v: View?, hasFocus: Boolean)
 class NativeOnFocusChangeListener(private val uniqueId: Int, private val viewId: Int): View.OnFocusChangeListener {
+    override fun onFocusChange(v: View?, hasFocus: Boolean) {
+        if (v != null) {
+            onFocusChangeExtended(uniqueId, v.id == viewId, v.id, v, hasFocus)
+        } else {
+            onFocusChange(uniqueId, hasFocus)
+        }
+    }
+
+    private external fun onFocusChange(uniqueId: Int, hasFocus: Boolean)
+    private external fun onFocusChangeExtended(uniqueId: Int, sameView: Boolean, vId: Int, v: View, hasFocus: Boolean)
 }
