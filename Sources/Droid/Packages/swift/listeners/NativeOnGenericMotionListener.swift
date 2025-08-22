08703bbf933cceb5976a2c703cf569d11609fbf5
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-22 05:08:01 +0400

    🪚 Implement `NativeOnGenericMotionListener`

---
+++ Sources/Droid/Packages/swift/listeners/NativeOnGenericMotionListener.swift
@@ -0,0 +1,133 @@
+//
+//  NativeOnGenericMotionListener.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 21.08.2025.
+//
+
+#if os(Android)
+import Android
+#if canImport(AndroidLooper)
+import AndroidLooper
+#endif
+
+extension AppKitPackage.ListenersPackage {
+    public class OnGenericMotionListenerClass: JClassName, @unchecked Sendable {}
+    public var OnGenericMotionListener: OnGenericMotionListenerClass { .init(parent: self, name: "NativeOnGenericMotionListener") }
+}
+
+final class NativeOnGenericMotionListener: NativeListener, AnyNativeListener, @unchecked Sendable {
+    /// The JNI class name
+    class var className: JClassName { "stream/swift/droid/appkit/listeners/NativeOnGenericMotionListener" }
+
+    var shouldInitWithViewId: Bool { true }
+
+    typealias Handler = (@UIThreadActor () -> Bool)
+    typealias HandlerWithEvent = (@UIThreadActor (NativeOnGenericMotionListenerEvent) -> Bool)
+
+    /// View
+    var view: View?
+
+    /// Handlers
+    var handler: Handler?
+    var handlerWithEvent: HandlerWithEvent?
+
+    func setHandler(_ view: View?, _ handler: @escaping Handler) -> Self {
+        self.view = view
+        self.handler = handler
+        return self
+    }
+
+    func setHandler(_ view: View?, _ handler: @escaping HandlerWithEvent) -> Self {
+        self.view = view
+        self.handlerWithEvent = handler
+        return self
+    }
+
+    #if canImport(AndroidLooper)
+    @UIThreadActor
+    func handle(_ isSameView: Bool, _ triggerView: NativeListenerTriggerView?, _ motionEvent: MotionEvent?) -> Bool {
+        if let view {
+            return handlerWithEvent?(.init(view, isSameView, triggerView, motionEvent)) ?? handler?() ?? false
+        } else {
+            return handler?() ?? false
+        }
+    }
+    #endif
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnGenericMotionListener_onGenericMotion")
+public func nativeListenerOnGenericMotion(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint) -> jboolean {
+    guard
+        let listener: NativeOnGenericMotionListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(true, nil, nil)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnGenericMotionListener_onGenericMotionView")
+public func nativeListenerOnGenericMotionView(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, sameView: jboolean, vId: jint, v: jobject) -> jboolean {
+    guard
+        let listener: NativeOnGenericMotionListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let bool = sameView == 1 ? true : false
+    var triggerView: NativeListenerTriggerView?
+    if let object = v.box(JEnv(env))?.object() {
+        triggerView = .init(id: vId, object: object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(bool, triggerView, nil)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnGenericMotionListener_onGenericMotionEvent")
+public func nativeListenerOnGenericMotionEvent(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, event: jobject) -> jboolean {
+    guard
+        let listener: NativeOnGenericMotionListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    var motionEvent: MotionEvent?
+    if let object = event.box(JEnv(env))?.object() {
+        motionEvent = .init(object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(true, nil, motionEvent)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnGenericMotionListener_onGenericMotionViewEvent")
+public func nativeListenerOnGenericMotionViewEvent(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, sameView: jboolean, vId: jint, v: jobject, event: jobject) -> jboolean {
+    guard
+        let listener: NativeOnGenericMotionListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let bool = sameView == 1 ? true : false
+    var triggerView: NativeListenerTriggerView?
+    if let object = v.box(JEnv(env))?.object() {
+        triggerView = .init(id: vId, object: object)
+    }
+    var motionEvent: MotionEvent?
+    if let object = event.box(JEnv(env))?.object() {
+        motionEvent = .init(object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(bool, triggerView, motionEvent)
+    }
+    return result ? 1 : 0
+}
+#endif
+
+public final class NativeOnGenericMotionListenerEvent {
+    public let view: View
+    public let isSameView: Bool
+    public let triggerView: NativeListenerTriggerView?
+    public let motionEvent: MotionEvent?
+    init (_ view: View, _ isSameView: Bool, _ triggerView: NativeListenerTriggerView?, _ motionEvent: MotionEvent?) {
+        self.view = view
+        self.isSameView = isSameView
+        self.triggerView = triggerView
+        self.motionEvent = motionEvent
+    }
+}
--- appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnGenericMotionListener.kt
+++ appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnGenericMotionListener.kt
@@ -3,6 +3,23 @@ package stream.swift.droid.appkit.listeners
 import android.view.MotionEvent
 import android.view.View
 
-    external override fun onGenericMotion(v: View?, event: MotionEvent?): Boolean
 class NativeOnGenericMotionListener(private val uniqueId: Int, private val viewId: Int): View.OnGenericMotionListener {
+    override fun onGenericMotion(v: View?, event: MotionEvent?): Boolean {
+        if (v != null) {
+            if (event != null) {
+                return onGenericMotionViewEvent(uniqueId, v.id == viewId, v.id, v, event)
+            } else {
+                return onGenericMotionView(uniqueId, v.id == viewId, v.id, v)
+            }
+        } else if (event != null) {
+            return onGenericMotionEvent(uniqueId, event)
+        } else {
+            return onGenericMotion(uniqueId)
+        }
+    }
+
+    private external fun onGenericMotion(uniqueId: Int): Boolean
+    private external fun onGenericMotionView(uniqueId: Int, sameView: Boolean, vId: Int, v: View): Boolean
+    private external fun onGenericMotionEvent(uniqueId: Int, event: MotionEvent?): Boolean
+    private external fun onGenericMotionViewEvent(uniqueId: Int, sameView: Boolean, vId: Int, v: View, event: MotionEvent): Boolean
 }
