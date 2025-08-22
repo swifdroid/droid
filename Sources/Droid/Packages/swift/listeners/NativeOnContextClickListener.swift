9882d840096a48f41c5709b091f8f29ac1ef85a8
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-22 05:00:22 +0400

    🪚 Implement `NativeOnContextClickListener`

---
+++ Sources/Droid/Packages/swift/listeners/NativeOnContextClickListener.swift
@@ -0,0 +1,96 @@
+//
+//  NativeOnContextClickListener.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 21.08.2025.
+//
+
+#if os(Android)
+import Android
+#if canImport(AndroidLooper)
+import AndroidLooper
+#endif
+
+extension AppKitPackage.ListenersPackage {
+    public class OnContextClickListenerClass: JClassName, @unchecked Sendable {}
+    public var OnContextClickListener: OnContextClickListenerClass { .init(parent: self, name: "NativeOnContextClickListener") }
+}
+
+final class NativeOnContextClickListener: NativeListener, AnyNativeListener, @unchecked Sendable {
+    /// The JNI class name
+    class var className: JClassName { "stream/swift/droid/appkit/listeners/NativeOnContextClickListener" }
+
+    var shouldInitWithViewId: Bool { true }
+
+    typealias Handler = (@UIThreadActor () -> Bool)
+    typealias HandlerWithEvent = (@UIThreadActor (NativeOnContextClickListenerEvent) -> Bool)
+
+    /// View
+    var view: View?
+
+    /// Handlers
+    var handler: Handler?
+    var handlerWithEvent: HandlerWithEvent?
+
+    func setHandler(_ view: View?, _ handler: @escaping Handler) -> Self {
+        self.view = view
+        self.handler = handler
+        return self
+    }
+
+    func setHandler(_ view: View?, _ handler: @escaping HandlerWithEvent) -> Self {
+        self.view = view
+        self.handlerWithEvent = handler
+        return self
+    }
+
+    #if canImport(AndroidLooper)
+    @UIThreadActor
+    func handle(_ isSameView: Bool, _ triggerView: NativeListenerTriggerView? = nil) -> Bool {
+        if let view {
+            return handlerWithEvent?(.init(view, isSameView, triggerView)) ?? handler?() ?? false
+        } else {
+            return handler?() ?? false
+        }
+    }
+    #endif
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnContextClickListener_onContextClick")
+public func nativeListenerOnContextClick(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint) -> jboolean {
+    guard
+        let listener: NativeOnContextClickListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(true, nil)
+    }
+    return result ? 1 : 0
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnContextClickListener_onContextClickExtended")
+public func nativeListenerOnContextClickExtended(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, sameView: jboolean, vId: jint, v: jobject) -> jboolean {
+    guard
+        let listener: NativeOnContextClickListener = ListenerStore.shared.find(id: uniqueId)
+    else { return 0 }
+    let bool = sameView == 1 ? true : false
+    var triggerView: NativeListenerTriggerView?
+    if let object = v.box(JEnv(env))?.object() {
+        triggerView = .init(id: vId, object: object)
+    }
+    let result = UIThreadActor.assumeIsolated {
+        listener.handle(bool, triggerView)
+    }
+    return result ? 1 : 0
+}
+#endif
+
+public final class NativeOnContextClickListenerEvent {
+    public let view: View
+    public let isSameView: Bool
+    public let triggerView: NativeListenerTriggerView?
+    init (_ view: View, _ isSameView: Bool, _ triggerView: NativeListenerTriggerView? = nil) {
+        self.view = view
+        self.isSameView = isSameView
+        self.triggerView = triggerView
+    }
+}
--- appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnContextClickListener.kt
+++ appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnContextClickListener.kt
@@ -2,6 +2,15 @@ package stream.swift.droid.appkit.listeners
 
 import android.view.View
 
-    external override fun onContextClick(v: View?): Boolean
 class NativeOnContextClickListener(private val uniqueId: Int, private val viewId: Int): View.OnContextClickListener {
+    override fun onContextClick(v: View?): Boolean {
+        if (v != null) {
+            return onContextClickExtended(uniqueId, v.id == viewId, v)
+        } else {
+            return onContextClick(uniqueId)
+        }
+    }
+
+    private external fun onContextClick(uniqueId: Int): Boolean
+    private external fun onContextClickExtended(uniqueId: Int, sameView: Boolean, v: View): Boolean
 }
