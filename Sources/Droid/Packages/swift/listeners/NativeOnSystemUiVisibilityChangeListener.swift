b352c37cc1f4a3a81835dc9b66acaed947213a9b
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-22 05:09:44 +0400

    🪚 Implement `NativeOnSystemUiVisibilityChangeListener`

---
+++ Sources/Droid/Packages/swift/listeners/NativeOnSystemUiVisibilityChangeListener.swift
@@ -0,0 +1,59 @@
+//
+//  NativeOnSystemUiVisibilityChangeListener.swift
+//  
+//
+//  Created by Mihael Isaev on 22.08.2025.
+//
+
+#if os(Android)
+import Android
+#if canImport(AndroidLooper)
+import AndroidLooper
+#endif
+
+extension AppKitPackage.ListenersPackage {
+    public class OnSystemUiVisibilityChangeListenerClass: JClassName, @unchecked Sendable {}
+    public var OnSystemUiVisibilityChangeListener: OnSystemUiVisibilityChangeListenerClass { .init(parent: self, name: "NativeOnSystemUiVisibilityChangeListener") }
+}
+
+final class NativeOnSystemUiVisibilityChangeListener: NativeListener, AnyNativeListener, @unchecked Sendable {
+    /// The JNI class name
+    class var className: JClassName { "stream/swift/droid/appkit/listeners/NativeOnSystemUiVisibilityChangeListener" }    
+
+    var shouldInitWithViewId: Bool { true }
+
+    typealias Handler = (@UIThreadActor (_ visibility: Int) -> Void)
+
+    /// View
+    var view: View?
+
+    /// Handlers
+    var handler: Handler?
+
+    func setHandler(_ view: View?, _ handler: @escaping Handler) -> Self {
+        self.view = view
+        self.handler = handler
+        return self
+    }
+
+    #if canImport(AndroidLooper)
+    @UIThreadActor
+    func handle(_ visibility: Int) {
+        handler?(visibility)
+    }
+    #endif
+}
+
+@_cdecl("Java_stream_swift_droid_appkit_listeners_NativeOnSystemUiVisibilityChangeListener_onSystemUiVisibilityChange")
+public func nativeListenerOnSystemUiVisibilityChange(env: UnsafeMutablePointer<JNIEnv?>, callerClassObject: jobject, uniqueId: jint, visibility: jint) {
+    guard
+        let listener: NativeOnSystemUiVisibilityChangeListener = ListenerStore.shared.find(id: uniqueId)
+    else { return }
+    let visibility = Int(visibility)
+    #if canImport(AndroidLooper)
+    Task { @UIThreadActor in
+        listener.handle(visibility)
+    }
+    #endif
+}
+#endif
--- appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnSystemUiVisibilityChangeListener.kt
+++ appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnSystemUiVisibilityChangeListener.kt
@@ -4,5 +4,9 @@ import android.view.View
 
 class NativeOnSystemUiVisibilityChangeListener(private val uniqueId: Int): View.OnSystemUiVisibilityChangeListener {
     @Deprecated("Deprecated in Java")
-    external override fun onSystemUiVisibilityChange(visibility: Int)
+    override fun onSystemUiVisibilityChange(visibility: Int) {
+        onSystemUiVisibilityChange(uniqueId, visibility)
+    }
+
+    private external fun onSystemUiVisibilityChange(uniqueId: Int, visibility: Int)
 }
