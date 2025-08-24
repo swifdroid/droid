31b7206805588a6fb2642be8f8cc622bb857af58
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-24 17:56:38 +0400

    🪚 Implement `appcompat.widget.Toolbar`

--- Sources/Droid/Packages/androidx/appcompat/widget/AppCompatWidgetToolbar.swift
+++ Sources/Droid/Packages/androidx/appcompat/widget/AppCompatWidgetToolbar.swift
@@ -25,3 +25,66 @@ public final class Toolbar: ViewGroup, @unchecked Sendable {
         super.init(content: content)
     }
 }
+
+// MARK: - Methods
+
+extension Toolbar {
+    /// Check whether this Toolbar is currently hosting an expanded action view.
+    public func hasExpandedActionView() -> Bool {
+        instance?.callBoolMethod(name: "hasExpandedActionView") ?? false
+    }
+}
+
+extension Toolbar {
+    /// Hide the overflow items from the associated menu.
+    public func hideOverflowMenu() -> Bool {
+        instance?.callBoolMethod(name: "hideOverflowMenu") ?? false
+    }
+}
+
+extension Toolbar {
+    /// Invalidates the to ensure that what is displayed matches the current internal state of the menu.
+    ///
+    /// This should be called whenever the state of the menu is changed, such as items being removed or disabled based on some user event.
+    /// Only the items in the Menu that were provided by `MenuProviders` should be removed and repopulated, leaving
+    /// all manually inflated menu items untouched, as they should continue to be managed manually.
+    public func invalidateMenu() {
+        instance?.callVoidMethod(name: "invalidateMenu")
+    }
+}
+
+extension Toolbar {
+    /// Returns whether the toolbar will attempt to register its own `OnBackInvokedCallback` in supported configurations
+    /// to handle collapsing expanded action items when a back invocation occurs.
+    public func isBackInvokedCallbackEnabled() -> Bool {
+        instance?.callBoolMethod(name: "isBackInvokedCallbackEnabled") ?? false
+    }
+}
+
+extension Toolbar {
+    /// Check whether the overflow menu is currently showing.
+    /// This may not reflect a pending show operation in progress.
+    public func isOverflowMenuShowing() -> Bool {
+        instance?.callBoolMethod(name: "isOverflowMenuShowing") ?? false
+    }
+}
+
+extension Toolbar {
+    public func onHoverEvent(_ event: MotionEvent) -> Bool {
+        instance?.callBoolMethod(name: "onHoverEvent", args: event.signed(as: MotionEvent.className)) ?? false
+    }
+}
+
+extension Toolbar {
+    public func onRtlPropertiesChanged(_ direction: LayoutDirection) {
+        instance?.callVoidMethod(name: "onRtlPropertiesChanged", args: direction.rawValue)
+    }
+}
+
+extension Toolbar {
+    public func onTouchEvent(_ event: MotionEvent) -> Bool {
+        instance?.callBoolMethod(name: "onTouchEvent", args: event.signed(as: MotionEvent.className)) ?? false
+    }
+}
+
+// TODO: implement removeMenuProvider
