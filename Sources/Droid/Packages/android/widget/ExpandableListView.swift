7d1fc3accc03d2fe1f10f7b432ad286364db7f38
Mikhail Isaev <isaev.mihael@gmail.com>
2025-08-28 04:34:27 +0400

    🪚 ExpandableListView: add description

--- Sources/Droid/Packages/android/widget/ExpandableListView.swift
+++ Sources/Droid/Packages/android/widget/ExpandableListView.swift
@@ -10,9 +10,18 @@ extension AndroidPackage.WidgetPackage {
     public var ExpandableListView: ExpandableListViewClass { .init(parent: self, name: "ExpandableListView") }
 }
 
+/// A view that shows items in a vertically scrolling two-level list.
+/// 
+/// This differs from the ListView by allowing two levels: groups which can individually be expanded to show its children.
+/// 
+/// The items come from the ExpandableListAdapter associated with this view.
+/// 
+/// [Learn more](https://developer.android.com/reference/android/widget/ExpandableListView)
 open class ExpandableListView: View, @unchecked Sendable {
     @discardableResult
     public override init (id: Int32? = nil) {
         super.init(id: id)
     }
+
+    // TODO
 }
