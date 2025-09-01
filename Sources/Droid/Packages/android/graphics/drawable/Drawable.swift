3896a1fd9a7ab54a097bd304fc0fed97cc222cc5
Mikhail Isaev <isaev.mihael@gmail.com>
2025-09-01 13:04:50 +0400

    🔧 Drawable: improve initial implementation

--- Sources/Droid/Packages/android/graphics/drawable/Drawable.swift
+++ Sources/Droid/Packages/android/graphics/drawable/Drawable.swift
@@ -5,20 +5,29 @@
 //  Created by Mihael Isaev on 15.01.2022.
 //
 
+#if canImport(AndroidLooper)
+import AndroidLooper
+#endif
+
 extension AndroidPackage.GraphicsPackage.DrawablePackage {
     public class DrawableClass: JClassName, @unchecked Sendable {}
     public var Drawable: DrawableClass { .init(parent: self, name: "Drawable") }
 }
 
-open class Drawable: JObjectable, @unchecked Sendable {
+#if canImport(AndroidLooper)
+@UIThreadActor
+#endif
+open class Drawable: JObjectable, Contextable, @unchecked Sendable {
     /// The JNI class name
     open class var className: JClassName { .android.graphics.drawable.Drawable }
     
     public let object: JObject
+    public let context: ActivityContext
 
     @discardableResult
-    public init(_ object: JObject) {
+    public init(_ object: JObject, _ context: Contextable) {
         self.object = object
+        self.context = context.context
     }
 
     // TODO: implement Drawable
