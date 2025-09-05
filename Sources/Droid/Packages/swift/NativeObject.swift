f3fce9b6a642e398739980feb2adf35e56cdfda4
Mikhail Isaev <isaev.mihael@gmail.com>
2025-09-05 07:00:29 +0400

    🪚 Implement `NativeObject`

---
+++ Sources/Droid/Packages/swift/NativeObject.swift
@@ -0,0 +1,148 @@
+//
+//  NativeObject.swift
+//  Droid
+//
+//  Created by Mihael Isaev on 29.08.2025.
+//
+
+#if os(Android)
+import Android
+#if canImport(AndroidLooper)
+import AndroidLooper
+#endif
+#endif
+
+protocol AnyNativeObject: AnyObject {
+    static var nativeObjectClassName: JClassName { get }
+    var id: Int32 { get }
+}
+
+extension AnyNativeObject {
+    var stringId: String { "\(Self.nativeObjectClassName.name)\(id)" }
+    static func stringId(_ id: Int32) -> String {
+        "\(nativeObjectClassName.name)\(id)"
+    }
+    
+    func addIntoStore() {
+        ObjectStore.shared.add(self)
+    }
+}
+
+#if canImport(AndroidLooper)
+@UIThreadActor
+#endif
+open class NativeUIObject: NativeObject, Contextable, @unchecked Sendable {
+    public let context: ActivityContext
+
+    public init (_ object: JObject, _ context: Contextable) {
+        self.context = context.context
+        super.init(object)
+    }
+
+    public convenience init? (_ context: Contextable, _ className: JClassName) {
+        guard let env = JEnv.current() else { return nil }
+        self.init(env, context, className)
+    }
+
+    public init? (_ env: JEnv, _ context: Contextable, _ className: JClassName) {
+        self.context = context.context
+        super.init(env, className)
+    }
+}
+
+open class NativeObject: JObjectable, @unchecked Sendable {
+    /// Unique identifier
+    let id: Int32
+
+    public let object: JObject
+
+    public init (_ object: JObject) {
+        self.id = DroidApp.shared.getNextViewId()
+        self.object = object
+    }
+
+    public convenience init? (_ className: JClassName) {
+        guard let env = JEnv.current() else { return nil }
+        self.init(env, className)
+    }
+
+    public init? (_ env: JEnv, _ className: JClassName) {
+        self.id = DroidApp.shared.getNextViewId()
+        #if os(Android)
+        let signature: [JSignatureItem] = [.int]
+        let args: [any JValuable] = [id]
+        guard
+            let clazz = JClass.load(className),
+            let methodId = clazz.methodId(env: env, name: "<init>", signature: .init(signature, returning: .void)),
+            let global = env.newObject(clazz: clazz, constructor: methodId, args: args)
+        else {
+            InnerLog.w("⚠️ Unable to initialize NativeObject for className: \(className.path)")
+            return nil
+        }
+        self.object = global
+        if let s = self as? AnyNativeObject {
+            s.addIntoStore()
+        } else {
+            InnerLog.w("⚠️ Unable to store \(className.path) because it does not conform to AnyNativeObject")
+        }
+        #else
+        return nil
+        #endif
+    }
+    
+    // deinit {
+    //     ObjectStore.shared.remove(id: id)
+    // }
+}
+
+// MARK: - Store
+
+final class ObjectStore: @unchecked Sendable {
+    static let shared = ObjectStore()
+
+    var objects: [String: AnyNativeObject] = [:]
+    #if os(Android)
+    var mutex = pthread_mutex_t()
+    #endif
+
+    init () {
+        #if os(Android)
+        mutex.activate(recursive: true)
+        #endif
+    }
+
+    deinit {
+        #if os(Android)
+        mutex.destroy()
+        #endif
+    }
+
+    func add(_ object: AnyNativeObject) {
+        #if os(Android)
+        mutex.lock()
+        defer { mutex.unlock() }
+        // InnerLog.t("🟥 ObjectStore add \(object.stringId)")
+        objects[object.stringId] = object
+        #endif
+    }
+
+    func remove(_ object: AnyNativeObject) {
+        #if os(Android)
+        mutex.lock()
+        defer { mutex.unlock() }
+        // InnerLog.t("🟥 ObjectStore remove \(object.stringId)")
+        objects[object.stringId] = nil
+        #endif
+    }
+
+    func find<O: AnyNativeObject>(id: Int32) -> O? {
+        #if os(Android)
+        mutex.lock()
+        defer { mutex.unlock() }
+        // InnerLog.t("🟥 ObjectStore find \(O.stringId(id))")
+        return objects[O.stringId(id)] as? O
+        #else
+        return nil
+        #endif
+    }
+}
