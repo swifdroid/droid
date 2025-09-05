7e866bd97d70d3beb66933ad50f1ba5c45e9a319
Mikhail Isaev <isaev.mihael@gmail.com>
2025-09-05 06:53:28 +0400

    🪚 Fragment: refactoring

--- Sources/Droid/Packages/androidx/fragment/app/Fragment.swift
+++ Sources/Droid/Packages/androidx/fragment/app/Fragment.swift
@@ -17,197 +17,90 @@ import AndroidLooper
 #if canImport(AndroidLooper)
 @UIThreadActor
 #endif
-open class Fragment: JObjectable, @unchecked Sendable {
-    public class var className: JClassName { .init(stringLiteral: "androidx/fragment/app/Fragment") }
+open class Fragment: NativeFragment, @unchecked Sendable {
+    /// The JNI class name
+    public static let className: JClassName = "androidx/fragment/app/Fragment"
 
-    public let object: JObject
+    var contentBlock: BodyBuilder.SingleView?
 
-    public init (_ object: JObject) {
-        self.object = object
+    /// Sets the content view for this fragment.
+    @discardableResult
+    public func body(@BodyBuilder block: @escaping BodyBuilder.SingleView) -> Self {
+        contentBlock = block
+        return self
     }
 
-    /// Called when a fragment is first attached to its context. `onCreate` will be called after this.
-    open func onAttach(_ context: ActivityContext) {} // TODO:
-    
-    open func onConfigurationChanged() {} // TODO: implement Configuration https://developer.android.com/reference/android/content/res/Configuration.html
-    
-    /// This hook is called whenever an item in a context menu is selected.
-    /// The default implementation simply returns false to have the normal processing happen
-    /// (calling the item's Runnable or sending a message to its Handler as appropriate).
-    /// You can use this method for any items for which you would like to do processing without those other facilities.
-    ///
-    /// Use getMenuInfo to get extra information set by the View that added this menu item.
-    ///
-    /// Derived classes should call through to the base class for it to perform the default menu handling.
-    open func onContextItemSelected(_ item: MenuItem) -> Bool { // TODO
-        return false
+    /// Builds the UI for this fragment.
+    open func buildUI() {}
+
+    /// The content view for this fragment.
+    var contentView: View?
+
+    open override func onCreateView(_ inflater: LayoutInflater, _ container: View?, _ savedInstanceState: Bundle?) -> View? {
+        buildUI()
+        func returnPlaceholder() -> View {
+            let contentView = TextView("Empty Fragment Content").gravity(.center)
+            if let container {
+                contentView.setStatusInParent(container, context)
+            } else {
+                contentView.setStatusAsContentView(context)
+            }
+            contentView.willMoveToParent()
+            return contentView
+        }
+        guard let contentBlock else {
+            return returnPlaceholder()
+        }
+        let item = contentBlock().bodyBuilderItem
+        func setDefaultFrameLayout(_ item: BodyBuilderItem) {
+            let view = FrameLayout()
+            view.addItem(item)
+            contentView = view
+        }
+        func proceedItem(_ item: BodyBuilderItem) {
+            switch item {
+            case .single(let view):
+                contentView = view
+            case .multiple(let views):
+                if views.count == 1, let view = views.first {
+                    contentView = view
+                } else {
+                    setDefaultFrameLayout(item)
+                }
+            case .nested(let items):
+                if items.count == 1, let item = items.first {
+                    proceedItem(item.bodyBuilderItem)
+                } else {
+                    setDefaultFrameLayout(item)
+                }
+            case .forEach:
+                setDefaultFrameLayout(item)
+            case .none:
+                setDefaultFrameLayout(item)
+            }
+        }
+        proceedItem(item)
+        guard let contentView else {
+            return returnPlaceholder()
+        }
+        if let container {
+            contentView.setStatusInParent(container, context)
+        } else {
+            contentView.setStatusAsContentView(context)
+        }
+        contentView.willMoveToParent()
+        return contentView
     }
 
-    /// Called to do initial creation of a fragment.
-    /// This is called after `onAttach` and before `onCreateView`.
-    ///
-    /// Note that this can be called while the fragment's activity is still in the process of being created.
-    /// As such, you can not rely on things like the activity's content view hierarchy being initialized at this point.
-    /// If you want to do work once the activity itself is created, add a `androidx.lifecycle.LifecycleObserver`
-    /// on the activity's Lifecycle, removing it when it receives the `CREATED` callback.
-    ///
-    /// Any restored child fragments will be created before the base `Fragment.onCreate` method returns.
-    open func onCreate(_ savedInstanceState: Bundle?) {} // TODO
-
-    /// Called when a fragment loads an animation.
-    /// 
-    /// Note that if `setCustomAnimations` was called with `Animator` resources
-    /// instead of Animation resources, nextAnim will be an animator resource.
-    ///
-    /// Return your custom `Animation` implementation or one of the following:
-    ///    - AlphaAnimation:	An animation that controls the alpha level of an object. 
-    ///    - AnimationSet:	Represents a group of Animations that should be played together. 
-    ///    - RotateAnimation:	An animation that controls the rotation of an object. 
-    ///    - ScaleAnimation:	An animation that controls the scale of an object. 
-    ///    - TranslateAnimation:	An animation that controls the position of an object.
-    open func onCreateAnimation(_ transit: Int32, _ enter: Bool, _ nextAnim: Int32) -> Animation? { // TODO:
-        return nil
+    open override func onViewCreated(view: View, savedInstanceState: Bundle?) {
+        if let contentView, let instance = contentView.instance {
+            if let lp = instance.layoutParams() {
+                contentView.processLayoutParams(instance, lp, for: contentView)
+                contentView.layoutParams(lp)
+            }
+            contentView.didMoveToParent()
+        }
     }
-
-    /// Called when a fragment loads an animator.
-    /// 
-    /// This will be called when `onCreateAnimation` returns null.
-    /// 
-    /// Note that if setCustomAnimations was called with `Animation` resources
-    /// instead of `Animator` resources, `nextAnim` will be an animation resource.
-    ///
-    /// Return your custom `Animator` implementation or one of the following:
-    ///    - AnimatorSet:	This class plays a set of Animator objects in the specified order. 
-    ///    - ValueAnimator:	This class provides a simple timing engine for running animations which calculate animated values and set them on target objects.
-    ///    - ObjectAnimator:	This subclass of ValueAnimator provides support for animating properties on target objects. 
-    ///    - TimeAnimator:	This class provides a simple callback mechanism to listeners that is synchronized with all other animators in the system. 
-    open func onCreateAnimator(_ transit: Int32, _ enter: Bool, _ nextAnim: Int32) -> Animator? { // TODO:
-        return nil
-    }
-
-    /// Called when a context menu for the view is about to be shown.
-    /// 
-    /// Unlike `onCreateOptionsMenu`, this will be called every time the context menu is about to be shown
-    /// and should be populated for the view (or item inside the view for AdapterView subclasses, this can be found in the menuInfo)).
-    /// 
-    /// Use `onContextItemSelected` to know when an item has been selected.
-    /// 
-    /// The default implementation calls up to Activity.onCreateContextMenu,
-    /// though you can not call this implementation if you don't want that behavior.
-    /// 
-    /// It is not safe to hold onto the context menu after this method returns.
-    open func onCreateContextMenu(_ menu: ContextMenu, _ v: View, _ menuInfo: ContextMenu.ContextMenuInfo?) {} // TODO
-    
-    /// Called to have the fragment instantiate its user interface view.
-    /// 
-    /// This is optional, and non-graphical fragments can return null.
-    /// 
-    /// This will be called between onCreate and `onViewCreated`.
-    /// 
-    /// A default View can be returned by calling Fragment in your constructor.
-    /// 
-    /// Otherwise, this method returns null.
-    /// 
-    /// It is recommended to only inflate the layout in this method and move logic that operates on the returned View to onViewCreated.
-    /// 
-    /// If you return a View from here, you will later be called in onDestroyView when the view is being released.
-    open func onCreateView(
-        _ inflater: LayoutInflater,
-        _ container: View?,
-        _ savedInstanceState: Bundle?
-    ) {} // TODO
-
-    /// Called when the fragment is no longer in use.
-    /// 
-    /// This is called after `onStop` and before `onDetach`.
-    open func onDestroy() {} // TODO
-
-    /// Called when the view previously created by `onCreateView` has been detached from the fragment.
-    /// 
-    /// The next time the fragment needs to be displayed, a new view will be created.
-    /// 
-    /// This is called after onStop and before `onDestroy`.
-    /// 
-    /// It is called regardless of whether `onCreateView` returned a non-null view.
-    /// 
-    /// Internally it is called after the view's state has been saved but before it has been removed from its parent.
-    open func onDestroyView() {} // TODO
-
-    /// Called when the fragment is no longer attached to its activity.
-    /// 
-    /// This is called after `onDestroy`.
-    open func onDetach() {}
-
-    // TODO: onGetLayoutInflater: not sure how to implement it
-
-    /// Called when the hidden state as returned by isHidden of the fragment or another fragment in its hierarchy has changed.
-    /// 
-    /// Fragments start out not hidden; this will be called whenever the fragment changes state from that.
-    open func onHiddenChanged(hidden: Bool) {} // TODO
-
-    /// Called when the Fragment's activity changes from fullscreen mode to multi-window mode and visa-versa.
-    /// 
-    /// This is generally tied to onMultiWindowModeChanged of the containing Activity.
-    open func onMultiWindowModeChanged(isInMultiWindowMode: Bool) {} // TODO
-
-    /// Called when the Fragment is no longer resumed.
-    /// 
-    /// This is generally tied to Activity.onPause of the containing Activity's lifecycle.
-    open func onPause() {} // TODO
-
-    /// Called by the system when the activity changes to and from picture-in-picture mode.
-    /// 
-    /// This is generally tied to onPictureInPictureModeChanged of the containing Activity.
-    open func onPictureInPictureModeChanged(isInPictureInPictureMode: Bool) {} // TODO
-
-    /// Callback for when the primary navigation state of this Fragment has changed.
-    /// This can be the result of the `getParentFragmentManager` containing FragmentManager}
-    /// having its primary navigation fragment changed via setPrimaryNavigationFragment
-    /// or due to the primary navigation fragment changing in a parent FragmentManager.
-    open func onPrimaryNavigationFragmentChanged(isPrimaryNavigationFragment: Bool) {} // TODO
-
-    /// Called when the fragment is visible to the user and actively running.
-    /// 
-    /// This is generally tied to Activity.onResume of the containing Activity's lifecycle.
-    open func onResume() {} // TODO
-
-    /// Called to ask the fragment to save its current dynamic state, so it can later be reconstructed in a new instance if its process is restarted.
-    /// 
-    /// If a new instance of the fragment later needs to be created, the data you place in the Bundle here will be available
-    /// in the Bundle given to onCreate, onCreateView, and onViewCreated.
-    ///
-    /// This corresponds to Activity.onSaveInstanceState(Bundle) and most of the discussion there applies here as well.
-    /// 
-    /// Note however: this method may be called at any time before onDestroy.
-    /// 
-    /// There are many situations where a fragment may be mostly torn down (such as when placed on the back stack with no UI showing),
-    /// but its state will not be saved until its owning activity actually needs to save its state.
-    open func onSaveInstanceState(outState: Bundle) {} // TODO
-
-    /// Called when the Fragment is visible to the user.
-    /// 
-    /// This is generally tied to Activity.onStart of the containing Activity's lifecycle.
-    open func onStart() {} // TODO
-
-    /// Called when the Fragment is no longer started.
-    /// 
-    /// This is generally tied to Activity.onStop of the containing Activity's lifecycle.
-    open func onStop() {} // TODO
-
-    /// Called immediately after onCreateView has returned, but before any saved state has been restored in to the view.
-    /// 
-    /// This gives subclasses a chance to initialize themselves once they know their view hierarchy has been completely created.
-    /// 
-    /// The fragment's view hierarchy is not however attached to its parent at this point.
-    open func onViewCreated(view: View, savedInstanceState: Bundle?) {} // TODO
-
-    /// Called when all saved state has been restored into the view hierarchy of the fragment.
-    /// 
-    /// This can be used to do initialization based on saved state that you are letting the view hierarchy track itself,
-    /// such as whether check box widgets are currently checked.
-    /// 
-    /// This is called after onViewCreated and before onStart.
-    open func onViewStateRestored(savedInstanceState: Bundle) {} // TODO
 
     /// Postpone the entering Fragment transition until startPostponedEnterTransition or executePendingTransactions has been called.
     /// 
