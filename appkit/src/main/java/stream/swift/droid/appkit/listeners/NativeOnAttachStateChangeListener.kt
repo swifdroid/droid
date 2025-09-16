34529c0c9c5c517aac002ba5be4232d7f02dc105
Mikhail Isaev <isaev.mihael@gmail.com>
2025-09-16 07:26:02 +0400

    🔧 Improve `NativeOnAttachStateChangeListener`

--- appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnAttachStateChangeListener.kt
+++ appkit/src/main/java/stream/swift/droid/appkit/listeners/NativeOnAttachStateChangeListener.kt
@@ -2,7 +2,14 @@ package stream.swift.droid.appkit.listeners
 
 import android.view.View
 
-    external override fun onViewAttachedToWindow(v: View)
-    external override fun onViewDetachedFromWindow(v: View)
 class NativeOnAttachStateChangeListener(private val uniqueId: Int, private val viewId: Int): View.OnAttachStateChangeListener {
+    override fun onViewAttachedToWindow(v: View) {
+        onViewAttachedToWindow(uniqueId, v.id == viewId, v.id, v)
+    }
+    override fun onViewDetachedFromWindow(v: View) {
+        onViewDetachedFromWindow(uniqueId, v.id == viewId, v.id, v)
+    }
+
+    private external fun onViewAttachedToWindow(uniqueId: Int, sameView: Boolean, vId: Int, v: View)
+    private external fun onViewDetachedFromWindow(uniqueId: Int, sameView: Boolean, vId: Int, v: View)
 }
