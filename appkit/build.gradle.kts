aacd43c6876e90ff322ce66dc1e55c0f9af5e051
Mikhail Isaev <isaev.mihael@gmail.com>
2025-11-19 13:18:16 +0400

    📦 Change `appkit` dependencies to `api`

--- appkit/build.gradle.kts
+++ appkit/build.gradle.kts
@@ -36,9 +36,9 @@ android {
 }
 dependencies {
     //noinspection UseTomlInstead
-    implementation("androidx.annotation:annotation:1.9.1")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    api("androidx.annotation:annotation:1.9.1")
+    api("androidx.appcompat:appcompat:1.7.1")
+    api("androidx.recyclerview:recyclerview:1.4.0")
 }
 
 publishing {
