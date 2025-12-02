607391c5fb85c7654d772ed3096542e8b55e553c
Mikhail Isaev <isaev.mihael@gmail.com>
2025-12-02 09:03:20 +0400

    🪚 Implement `NestedScrollView`

--- Sources/Droid/Packages/androidx/core/widget/NestedScrollView.swift
+++ Sources/Droid/Packages/androidx/core/widget/NestedScrollView.swift
@@ -11,12 +11,78 @@ extension AndroidXPackage.CorePackage.WidgetPackage {
     public var NestedScrollView: NestedScrollViewClass { .init(parent: self, name: "NestedScrollView") }
 }
 
-// class NestedScrollView: View, @unchecked Sendable {
-//     override init (_ environment: JEnvironment, _ context: JObjectReference) {
-//         super.init(environment, context, classes: [.androidx.core.widget.NestedScrollView], args: [])
-//     }
-    
-//     required init(_ environment: JEnvironment, _ ref: JClassReference, _ object: jobject) {
-//         super.init(environment, ref, object)
-//     }
-// }
+/// NestedScrollView is just like `ScrollView`, but it supports acting as both
+/// a nested scrolling parent and child on both new and old versions of Android.
+/// 
+/// Nested scrolling is enabled by default.
+/// 
+/// [Learn more](https://developer.android.com/reference/androidx/core/widget/NestedScrollView)
+public final class NestedScrollView: FrameLayout, @unchecked Sendable {
+    /// The JNI class name
+    public override class var className: JClassName { .androidx.core.widget.NestedScrollView }
+
+    @discardableResult
+    public override init (id: Int32? = nil) {
+        super.init(id: id)
+    }
+
+    @discardableResult
+    public override init (id: Int32? = nil, @BodyBuilder content: BodyBuilder.SingleView) {
+        super.init(id: id, content: content)
+    }
+}
+
+// TODO: arrowScroll
+// TODO: computeScroll
+// TODO: dispatchKeyEvent
+// TODO: dispatchNestedFling
+// TODO: dispatchNestedPreFling
+// TODO: dispatchNestedPreScroll
+// TODO: dispatchNestedPreScroll
+// TODO: dispatchNestedScroll
+// TODO: executeKeyEvent
+// TODO: fling
+// TODO: fullScroll
+// TODO: getMaxScrollAmount
+// TODO: getNestedScrollAxes
+// TODO: hasNestedScrollingParent
+// TODO: isFillViewport
+// TODO: isNestedScrollingEnabled
+// TODO: isSmoothScrollingEnabled
+// TODO: onInterceptTouchEvent
+// TODO: onNestedFling
+// TODO: onNestedPreFling
+// TODO: onNestedPreScroll
+// TODO: onNestedScroll
+// TODO: onNestedScrollAccepted
+// TODO: onStartNestedScroll
+// TODO: onStopNestedScroll
+// TODO: onTouchEvent
+// TODO: pageScroll
+// TODO: requestChildFocus
+// TODO: requestChildRectangleOnScreen
+// TODO: requestDisallowInterceptTouchEvent
+// TODO: requestLayout
+// TODO: scrollTo
+// TODO: setFillViewport
+// TODO: setNestedScrollingEnabled
+// TODO: setOnScrollChangeListener
+// TODO: setSmoothScrollingEnabled
+// TODO: shouldDelayChildPressedState
+// TODO: smoothScrollBy
+// TODO: smoothScrollTo
+// TODO: startNestedScroll
+// TODO: stopNestedScroll
+// TODO: computeScrollDeltaToGetChildRectOnScreen
+// TODO: getBottomFadingEdgeStrength
+// TODO: getTopFadingEdgeStrength
+// TODO: measureChild
+// TODO: measureChildWithMargins
+// TODO: onLayout
+// TODO: onMeasure
+// TODO: onOverScrolled
+// TODO: onRequestFocusInDescendants
+// TODO: onRestoreInstanceState
+// TODO: onSaveInstanceState
+// TODO: onScrollChanged
+// TODO: onSizeChanged
