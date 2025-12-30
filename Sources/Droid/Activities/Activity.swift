60f77850908a1f57603ca2d5e604cd9d860571ba
Mikhail Isaev <isaev.mihael@gmail.com>
2025-12-30 19:19:34 +0400

    ✍️ Activity: add documentation comments

--- Sources/Droid/Activities/Activity.swift
+++ Sources/Droid/Activities/Activity.swift
@@ -122,6 +122,10 @@ open class Activity: Contextable, AnyActivity, Sendable {
         }
     }
 
+    /// Sets the activity content to an explicit view.
+    ///
+    /// If you want to further configure the view after setting it as content view,
+    /// you can use the `proceed` closure.
     public func contentView(_ view: View, _ proceed: ((View) -> Void)? = nil) {
         contentView = view
         proceed?(contentView!)
@@ -143,8 +147,15 @@ open class Activity: Contextable, AnyActivity, Sendable {
         view.didMoveToParent()
     }
 
+    /// it is called under the hood from **`super.onCreate()`** before `buildUI()`.
     @BodyBuilder open var body: BodyBuilder.Result { EmptyBodyBuilderItem() }
 
+    /// Declarative way to add views to the activity.
+    ///
+    /// If you declare one view at the root level, it will be used as the content view directly.
+    /// 
+    /// If you declare multiple views at the root level, a **`FrameLayout`**
+    /// will be created as the content view and the views will be added to it.
     @discardableResult
     open func body(@BodyBuilder block: BodyBuilder.SingleView) -> Self {
         InnerLog.t("activity body 1")
@@ -192,18 +203,75 @@ open class Activity: Contextable, AnyActivity, Sendable {
         return self
     }
 
+    /// it is called under the hood from **`super.onCreate()`** after `body()`.
     open func buildUI() {}
 
     // MARK: Lifecycle
 
     private var requestApplyInsetsAfterRestart = false
 
+    /// Called to retrieve per-instance state from an activity before being killed
+    /// so that the state can be restored in `onCreate()` or `onRestoreInstanceState()`
     open func onSaveInstanceState(bundle: Bundle) {
         context = nil
     }
+    /// The system calls this method as the first indication that the user is leaving your activity,
+    /// though it does not always mean the activity is being destroyed.
+    /// It indicates that the activity is no longer in the foreground,
+    /// but it is still visible if the user is in multi-window mode.
+    /// There are several reasons why an activity might enter this state:
+    /// 
+    /// - An event that interrupts app execution, as described in the section
+    /// about the `onResume()` callback, pauses the current activity. This is the most common case.
+    /// - In multi-window mode, only one app has focus at any time, and the system pauses all the other apps.
+    /// - The opening of a new, semi-transparent activity, such as a dialog,
+    /// pauses the activity it covers. As long as the activity is partially visible but not in focus, it remains paused.
+    /// 
+    /// When an activity moves to the `Paused` state, any lifecycle-aware component
+    /// tied to the activity's lifecycle receives the `ON_PAUSE` event.
+    /// This is where the lifecycle components can stop any functionality
+    /// that does not need to run while the component is not in the foreground,
+    /// such as stopping a camera preview.
+    /// 
+    /// Use the `onPause()` method to pause or adjust operations that can't continue,
+    /// or might continue in moderation, while the Activity is in the `Paused` state, and that you expect to resume shortly.
+    /// 
+    /// You can also use the `onPause()` method to release system resources,
+    /// handles to sensors (like GPS), or any resources that affect battery life
+    /// while your activity is `Paused` and the user does not need them.
+    /// 
+    /// However, as mentioned in the section about `onResume()`, a `Paused`
+    /// activity might still be fully visible if the app is in multi-window mode.
+    /// Consider using `onStop()` instead of `onPause()` to fully release
+    /// or adjust UI-related resources and operations to better support multi-window mode.
     open func onPause() {}
+    /// Called after `onSaveInstanceState()` when the activity is being re-initialized
+    /// after previously being shut down.
 	open func onStateNotSaved() {}
+    /// When the activity enters the `Resumed` state, it comes to the foreground,
+    /// and the system invokes the `onResume()` callback.
+    /// This is the state in which the app interacts with the user.
+    /// The app stays in this state until something happens to take focus away from the app,
+    /// such as the device receiving a phone call, the user navigating to another activity,
+    /// or the device screen turning off.
+    /// 
+    /// When the activity moves to the Resumed state, any lifecycle-aware component
+    /// tied to the activity's lifecycle receives the `ON_RESUME` event.
+    /// This is where the lifecycle components can enable any functionality that needs
+    /// to run while the component is visible and in the foreground, such as starting a camera preview.
+    /// 
+    /// When an interruptive event occurs, the activity enters the Paused state
+    /// and the system invokes the `onPause()` callback.
+    /// 
+    /// If the activity returns to the Resumed state from the Paused state,
+    /// the system once again calls the `onResume()` method.
+    /// For this reason, implement `onResume()` to initialize components that you release
+    /// during `onPause()` and to perform any other initializations that must occur
+    /// each time the activity enters the `Resumed` state.
 	open func onResume() {}
+    /// Called after `onRestart()` and before `onResume()`.
+    /// 
+    /// In this method, `requestApplyInsets` is called on `contentView` or `decorView` (if available) after `onRestart()`.
     open func onPostResume() {
         InnerLog.t("🟢 onPostResume called, requestApplyInsetsAfterRestart: \(requestApplyInsetsAfterRestart)")
         if requestApplyInsetsAfterRestart {
@@ -231,17 +299,87 @@ open class Activity: Contextable, AnyActivity, Sendable {
         InnerLog.t("🟢 onRestart called, will request apply insets after restart")
         requestApplyInsetsAfterRestart = true
     }
+    /// When the activity enters the `Started` state, the system invokes `onStart()`.
+    /// 
+    /// This call makes the activity visible to the user as the app prepares for
+    /// the activity to enter the foreground and become interactive.
+    /// 
+    /// For example, this method is where the code that maintains the UI is initialized.
+    ///
+    /// The `onStart()` method completes quickly and, as with the `Created` state,
+    /// the activity does not remain in the `Started` state.
+    /// Once this callback finishes, the activity enters the `Resumed` state
+    /// and the system invokes the `onResume()` method.
 	open func onStart() {}
+    /// When your activity is no longer visible to the user, it enters the `Stopped` state,
+    /// and the system invokes the `onStop()` callback. This can occur
+    /// when a newly launched activity covers the entire screen.
+    /// The system also calls `onStop()` when the activity finishes
+    /// running and is about to be terminated.
+    /// 
+    /// When the activity moves to the `Stopped` state, any lifecycle-aware component
+    /// tied to the activity's lifecycle receives the `ON_STOP` event.
+    /// This is where the lifecycle components can stop any functionality
+    /// that does not need to run while the component is not visible on the screen.
+    /// 
+    /// In the `onStop()` method, release or adjust resources that are not needed
+    /// while the app is not visible to the user. For example, your app might pause
+    /// animations or switch from fine-grained to coarse-grained location updates.
+    /// Using `onStop()` instead of `onPause()` means that UI-related work continues,
+    /// even when the user is viewing your activity in multi-window mode.
+    /// 
+    /// Also, use `onStop()` to perform relatively CPU-intensive shutdown operations.
+    /// For example, if you can't find a better time to save information to a database,
+    /// you might do so during `onStop()`.
 	open func onStop() {}
+    /// `onDestroy()` is called before the activity is destroyed.
+    /// The system invokes this callback for one of two reasons:
+    /// 
+    /// - The activity is finishing, due to the user completely dismissing
+    /// the activity or due to `finish()` being called on the activity.
+    /// - The system is temporarily destroying the activity due to a configuration change,
+    /// such as device rotation or entering multi-window mode.
+    /// 
+    /// When the activity moves to the destroyed state, any lifecycle-aware component
+    /// tied to the activity's lifecycle receives the `ON_DESTROY` event.
+    /// This is where the lifecycle components can clean up anything they need to before the Activity is destroyed.
+    /// 
+    /// Instead of putting logic in your Activity to determine why it is being destroyed,
+    /// use a `ViewModel` object to contain the relevant view data for your Activity.
+    /// If the Activity is recreated due to a configuration change,
+    /// the `ViewModel` does not have to do anything, since it is preserved
+    /// and given to the next Activity instance.
+    /// 
+    /// If the Activity isn't recreated, then the `ViewModel` has the `onCleared()` method called,
+    /// where it can clean up any data it needs to before being destroyed.
+    /// You can distinguish between these two scenarios with the `isFinishing()` method.
+    /// 
+    /// If the activity is finishing, `onDestroy()` is the final lifecycle callback the activity receives.
+    /// If `onDestroy()` is called as the result of a configuration change,
+    /// the system immediately creates a new activity instance and
+    /// then calls `onCreate()` on that new instance in the new configuration.
+    /// 
+    /// The `onDestroy()` callback releases all resources not released by earlier callbacks, such as `onStop()`.
+    ///
+    /// In this method `contentView` and `context` are set to `nil` to release references.
 	open func onDestroy() {
         contentView?.removeFromParent()
         contentView = nil
         context = nil
     }
+    /// Called when the activity has been attached to the window.
 	open func onAttachedToWindow() {}
-	open func onBackPressed() {}
-	open func onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?, componentCaller: ComponentCaller?) {}
+	/// Called when the user presses the back button.
+    open func onBackPressed() {}
+	/// Called when an activity you launched exits, giving you the `requestCode`
+    /// you started it with, the `resultCode` it returned, and any additional data from it
+    /// as an `Intent`.
+    open func onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?, componentCaller: ComponentCaller?) {}
+    /// Called when the activity is entering multi-window mode.
     open func onMultiWindowModeChanged(isInMultiWindowMode: Bool) {}
+    /// Callback for the result from requesting permissions.
+    /// The `results` parameter is an array of `ActivityPermissionResult`
+    /// corresponding to the requested permissions.
     open func onRequestPermissionsResult(requestCode: Int, results: [ActivityPermissionResult], deviceId: Int) {}
 
     // MARK: Methods
