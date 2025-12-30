acb4e040142c31c0c8171788aafc3e2ed8cd2d88
Mikhail Isaev <isaev.mihael@gmail.com>
2025-12-30 23:46:01 +0400

    ✍️ ActivityContext: add documentation comments

--- Sources/Droid/Activities/Activity.swift
+++ Sources/Droid/Activities/Activity.swift
@@ -458,6 +458,12 @@ public final class ComponentCaller: JObjectable, @unchecked Sendable {
     }
 }
 
+/// Interface to global information about an application environment.
+/// 
+/// This is an abstract class whose implementation is provided by the Android system.
+/// It allows access to application-specific resources and classes,
+/// as well as up-calls for application-level operations such as launching activities,
+/// broadcasting and receiving intents, etc.
 public final class ActivityContext: Contextable, JObjectable, JClassLoadable, @unchecked Sendable {
     public let object: JObject
 
